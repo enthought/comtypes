695f85b84e068175443eb0283d9bcc032745ba25
thomas.heller <none@none>
2006-08-02 18:29:57 +0000

    Don't pollute the namespace with too many symbols.
    --HG--
    extra : convert_revision : svn%3Aa2f44796-8cc0-49ac-b43f-6a96d556d52d/trunk%4013

--- comtypes/server/__init__.py
+++ comtypes/server/__init__.py
@@ -1,16 +1,17 @@
-from ctypes import *
-from comtypes import IUnknown, GUID, STDMETHOD, HRESULT
+import comtypes, ctypes
 
 ################################################################
 # Interfaces
-class IClassFactory(IUnknown):
-    _iid_ = GUID("{00000001-0000-0000-C000-000000000046}")
+class IClassFactory(comtypes.IUnknown):
+    _iid_ = comtypes.GUID("{00000001-0000-0000-C000-000000000046}")
     _methods_ = [
-        STDMETHOD(HRESULT, "CreateInstance", [c_int, POINTER(GUID), POINTER(c_ulong)]),
-        STDMETHOD(HRESULT, "LockServer", [c_int])]
+        comtypes.STDMETHOD(comtypes.HRESULT, "CreateInstance",
+                           [ctypes.c_int, ctypes.POINTER(comtypes.GUID), ctypes.POINTER(ctypes.c_ulong)]),
+        comtypes.STDMETHOD(comtypes.HRESULT, "LockServer",
+                           [ctypes.c_int])]
 
-class IExternalConnection(IUnknown):
-    _iid_ = GUID("{00000019-0000-0000-C000-000000000046}")
-    _methods_ = [
-        STDMETHOD(HRESULT, "AddConnection", [c_ulong, c_ulong]),
-        STDMETHOD(HRESULT, "ReleaseConnection", [c_ulong, c_ulong, c_ulong])]
+##class IExternalConnection(IUnknown):
+##    _iid_ = GUID("{00000019-0000-0000-C000-000000000046}")
+##    _methods_ = [
+##        STDMETHOD(HRESULT, "AddConnection", [c_ulong, c_ulong]),
+##        STDMETHOD(HRESULT, "ReleaseConnection", [c_ulong, c_ulong, c_ulong])]
