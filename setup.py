b9b37127bc84c0ddab0f9569fe9fdf7fb3d6e8bc
thomas.heller <none@none>
2006-08-03 18:28:44 +0000

    Slight changes to the metadata, and add the .idl/.tlb files as package_data.
    --HG--
    extra : convert_revision : svn%3Aa2f44796-8cc0-49ac-b43f-6a96d556d52d/trunk%4029

--- setup.py
+++ setup.py
@@ -1,12 +1,7 @@
-r"""comtypes - pure Python COM package, based on the ctypes FFI library
+r"""comtypes - Python COM package, based on the ctypes FFI library.
 
-comtypes offers superior support for custom COM interfaces.
-
-Currently only COM client code is implemented, server support
-will follow later.
-
-Limitations:
- - dispinterface support is somewhat weak.
+comtypes allows to define, call, and implement custom COM interfaces
+in pure Python.
 
 ----------------------------------------------------------------"""
 VERSION = "0.3.0"
@@ -16,23 +11,27 @@ from distutils.core import setup
 ##import comtypes
 
 classifiers = [
-    'Development Status :: 3 - Alpha',
-##    'Development Status :: 4 - Beta',
+##    'Development Status :: 3 - Alpha',
+    'Development Status :: 4 - Beta',
 ##    'Development Status :: 5 - Production/Stable',
     'Intended Audience :: Developers',
     'License :: OSI Approved :: MIT License',
     'Operating System :: Microsoft :: Windows',
+    'Operating System :: Microsoft :: Windows CE',
     'Programming Language :: Python',
     'Topic :: Software Development :: Libraries :: Python Modules',
     ]
 
 setup(name="comtypes",
-      description="pure Python COM package, based on the ctypes FFI library",
+      description="Pure Python COM package, based on the ctypes package",
       long_description = __doc__,
       author="Thomas Heller",
       author_email="theller@python.net",
       license="MIT License",
       url="http://starship.python.net/crew/theller/comtypes/",
+
+      package_data = {"comtypes.test": ["TestComServer.idl",
+                                        "TestComServer.tlb"]},
 
       classifiers=classifiers,
       
