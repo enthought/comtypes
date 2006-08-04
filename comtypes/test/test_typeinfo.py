3a154777d7eb26a5b034bf91a1d758928d6e3ad0
thomas.heller <none@none>
2006-08-04 14:19:24 +0000

    Skip this test on Windows CE.
    --HG--
    extra : convert_revision : svn%3Aa2f44796-8cc0-49ac-b43f-6a96d556d52d/trunk%4035

--- comtypes/test/test_typeinfo.py
+++ comtypes/test/test_typeinfo.py
@@ -1,3 +1,4 @@
+import os
 import unittest as ut
 from ctypes import POINTER, byref
 from comtypes import GUID, COMError
@@ -5,66 +6,69 @@ from comtypes.automation import DISPATCH_METHOD
 from comtypes.typeinfo import LoadTypeLibEx, LoadRegTypeLib, \
      QueryPathOfRegTypeLib, TKIND_INTERFACE, TKIND_DISPATCH, TKIND_ENUM
 
-class Test(ut.TestCase):
-    def test_LoadTypeLibEx(self):
-        self.assertRaises(WindowsError, lambda: LoadTypeLibEx("<xxx.xx>"))
-        tlib = LoadTypeLibEx("shdocvw.dll")
-        self.failUnless(tlib.GetTypeInfoCount())
-        tlib.GetDocumentation(-1)
-        self.failUnlessEqual(tlib.IsName("iwebbrowser"), "IWebBrowser")
-        self.failUnlessEqual(tlib.IsName("IWEBBROWSER"), "IWebBrowser")
-        self.failUnless(tlib.FindName("IWebBrowser"))
-        self.failUnlessEqual(tlib.IsName("Spam"), None)
-        tlib.GetTypeComp()
-        
-        attr = tlib.GetLibAttr()
-        info = attr.guid, attr.wMajorVerNum, attr.wMinorVerNum
-        other_tlib = LoadRegTypeLib(*info)
-        self.failUnlessEqual(tlib, other_tlib)
+# We should add other test cases for Windows CE.
+if os.name == "nt":
+    class Test(ut.TestCase):
+        # No LoadTypeLibEx on windows ce
+        def test_LoadTypeLibEx(self):
+            self.assertRaises(WindowsError, lambda: LoadTypeLibEx("<xxx.xx>"))
+            tlib = LoadTypeLibEx("shdocvw.dll")
+            self.failUnless(tlib.GetTypeInfoCount())
+            tlib.GetDocumentation(-1)
+            self.failUnlessEqual(tlib.IsName("iwebbrowser"), "IWebBrowser")
+            self.failUnlessEqual(tlib.IsName("IWEBBROWSER"), "IWebBrowser")
+            self.failUnless(tlib.FindName("IWebBrowser"))
+            self.failUnlessEqual(tlib.IsName("Spam"), None)
+            tlib.GetTypeComp()
 
-##         for n in dir(attr):
-##             if not n.startswith("_"):
-##                 print "\t", n, getattr(attr, n)
+            attr = tlib.GetLibAttr()
+            info = attr.guid, attr.wMajorVerNum, attr.wMinorVerNum
+            other_tlib = LoadRegTypeLib(*info)
+            self.failUnlessEqual(tlib, other_tlib)
 
-        for i in range(tlib.GetTypeInfoCount()):
-            ti = tlib.GetTypeInfo(i)
-            ti.GetTypeAttr()
-            tlib.GetDocumentation(i)
-            tlib.GetTypeInfoType(i)
+    ##         for n in dir(attr):
+    ##             if not n.startswith("_"):
+    ##                 print "\t", n, getattr(attr, n)
 
-            c_tlib, index = ti.GetContainingTypeLib()
-            self.failUnlessEqual(c_tlib, tlib)
-            self.failUnlessEqual(index, i)
+            for i in range(tlib.GetTypeInfoCount()):
+                ti = tlib.GetTypeInfo(i)
+                ti.GetTypeAttr()
+                tlib.GetDocumentation(i)
+                tlib.GetTypeInfoType(i)
 
-        guid_null = GUID()
-        self.assertRaises(COMError, lambda: tlib.GetTypeInfoOfGuid(guid_null))
+                c_tlib, index = ti.GetContainingTypeLib()
+                self.failUnlessEqual(c_tlib, tlib)
+                self.failUnlessEqual(index, i)
 
-        self.failUnless(tlib.GetTypeInfoOfGuid(GUID("{EAB22AC1-30C1-11CF-A7EB-0000C05BAE0B}")))
+            guid_null = GUID()
+            self.assertRaises(COMError, lambda: tlib.GetTypeInfoOfGuid(guid_null))
 
-        path = QueryPathOfRegTypeLib(*info)
-        path = path.split("\0")[0]
-        self.failUnless(path.lower().endswith("shdocvw.dll"))
+            self.failUnless(tlib.GetTypeInfoOfGuid(GUID("{EAB22AC1-30C1-11CF-A7EB-0000C05BAE0B}")))
 
-    def test_TypeInfo(self):
-        tlib = LoadTypeLibEx("shdocvw.dll")
-        for index in range(tlib.GetTypeInfoCount()):
-            ti = tlib.GetTypeInfo(index)
-            ta = ti.GetTypeAttr()
-            ti.GetDocumentation(-1)
-            if ta.typekind in (TKIND_INTERFACE, TKIND_DISPATCH):
-                if ta.cImplTypes:
-                    href = ti.GetRefTypeOfImplType(0)
-                    base = ti.GetRefTypeInfo(href)
-                    base.GetDocumentation(-1)
-                    ti.GetImplTypeFlags(0)
-            for f in range(ta.cFuncs):
-                fd = ti.GetFuncDesc(f)
-                names = ti.GetNames(fd.memid, 32)
-                ti.GetIDsOfNames(*names)
-                ti.GetMops(fd.memid)
-            
-            for v in range(ta.cVars):
-                ti.GetVarDesc(v)
+            path = QueryPathOfRegTypeLib(*info)
+            path = path.split("\0")[0]
+            self.failUnless(path.lower().endswith("shdocvw.dll"))
+
+        def test_TypeInfo(self):
+            tlib = LoadTypeLibEx("shdocvw.dll")
+            for index in range(tlib.GetTypeInfoCount()):
+                ti = tlib.GetTypeInfo(index)
+                ta = ti.GetTypeAttr()
+                ti.GetDocumentation(-1)
+                if ta.typekind in (TKIND_INTERFACE, TKIND_DISPATCH):
+                    if ta.cImplTypes:
+                        href = ti.GetRefTypeOfImplType(0)
+                        base = ti.GetRefTypeInfo(href)
+                        base.GetDocumentation(-1)
+                        ti.GetImplTypeFlags(0)
+                for f in range(ta.cFuncs):
+                    fd = ti.GetFuncDesc(f)
+                    names = ti.GetNames(fd.memid, 32)
+                    ti.GetIDsOfNames(*names)
+                    ti.GetMops(fd.memid)
+
+                for v in range(ta.cVars):
+                    ti.GetVarDesc(v)
 
 if __name__ == "__main__":
     unittest.main()
