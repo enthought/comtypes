cb931830679afd24fa28ac03e4914292c8ef38ca
thomas.heller <none@none>
2006-09-05 12:30:30 +0000

    More tests for COM properties.
    --HG--
    extra : convert_revision : svn%3Aa2f44796-8cc0-49ac-b43f-6a96d556d52d/trunk%4043

--- comtypes/test/test_wmi.py
+++ comtypes/test/test_wmi.py
@@ -27,16 +27,23 @@ class Test(ut.TestCase):
             # .Value is a property with "[out] POINTER(VARIANT)" parameter.
             a = item.Properties_["Caption"].Value
             b = item.Properties_.Item("Caption").Value
+            c = item.Properties_("Caption").Value
             self.failUnlessEqual(a, b)
+            self.failUnlessEqual(a, c)
             self.failUnless(isinstance(a, basestring))
             self.failUnless(isinstance(b, basestring))
-##            for prop in item.Properties_:
-##                    print "\t", (prop.Name, prop.Value)
+            self.failUnless(isinstance(c, basestring))
+            result = {}
+            for prop in item.Properties_:
+                self.failUnless(isinstance(prop.Name, basestring))
+                prop.Value
+                result[prop.Name] = prop.Value
+##                print "\t", (prop.Name, prop.Value)
+            self.failUnlessEqual(len(item.Properties_), item.Properties_.Count)
+            self.failUnlessEqual(len(item.Properties_), len(result))
             self.failUnless(isinstance(item.Properties_["Description"].Value, unicode))
         # len(obj) is forwared to obj.Count
-        c1 = len(disks)
-        c2 = disks.Count
-        self.failUnlessEqual(c1, c2)
+        self.failUnlessEqual(len(disks), disks.Count)
 
 if __name__ == "__main__":
-    unittest.main()
+    ut.main()
