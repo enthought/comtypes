04ce9c15da63e6f5430d3ffdbcba30edee2b66f1
thomas.heller <none@none>
2006-12-01 19:58:02 +0000

    Changed rst2html flags used.
    --HG--
    extra : convert_revision : svn%3Aa2f44796-8cc0-49ac-b43f-6a96d556d52d/trunk%4057

--- docs/make.bat
+++ docs/make.bat
@@ -2,6 +2,12 @@
 rem Build the comtypes html pages
 
 set CSS=--stylesheet-path=ctypes.css
-set CMD=c:\python24\python -u c:\python24\scripts\rst2html.py %CSS%
+REM set OPTIONS=--time --source-link --initial-header-level=2
+set OPTIONS=--time --source-link
+set CMD=c:\python24\python -u c:\python24\scripts\rst2html.py %OPTIONS% %CSS%
 
-%CMD% comtypes.txt comtypes.html
+%CMD% comtypes.client.txt comtypes.client.html
+%CMD% com_interfaces.txt com_interfaces.html
+
+REM start com_interfaces.html
+REM start comtypes.client.html
