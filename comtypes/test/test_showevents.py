675992bab6f2b6bcdfa0c76780d2de80cdc03eb4
thomas.heller <none@none>
2006-12-07 07:31:48 +0000

    Add another way to pump messages with a timeout.
    --HG--
    extra : convert_revision : svn%3Aa2f44796-8cc0-49ac-b43f-6a96d556d52d/trunk%4067

--- comtypes/test/test_showevents.py
+++ comtypes/test/test_showevents.py
@@ -12,12 +12,7 @@ class EventsTest(unittest.TestCase):
     def tearDown(self):
         sys.stdout = self.old_stdout
 
-    def test(self):
-        o = comtypes.client.CreateObject("InternetExplorer.Application")
-        conn = comtypes.client.ShowEvents(o)
-        o.Quit()
-        del o
-
+    def pump_messages_with_timeout(self, timeout):
         # The following code waits for 'timeout' milliseconds in the
         # way required for COM, internally doing the correct things
         # depending on the COM appartment of the current thread.
@@ -34,6 +29,36 @@ class EventsTest(unittest.TestCase):
         except WindowsError, details:
             if details[0] != RPC_S_CALLPENDING: # timeout expired
                 raise
+
+    def pump_messages_with_timeout(self, timeout):
+        # Running a message loop with a timeout should work for all
+        # possible COM apartment types; however it is *required* for
+        # single threaded appartments.
+        from comtypes.server.localserver import pump_messages
+        def post_quit_message(hwnd, msg, idEvent, dwTime):
+            ctypes.windll.user32.PostQuitMessage(0)
+            ctypes.windll.user32.KillTimer(hwnd, idEvent)
+            return 0
+        TIMERPROC = ctypes.WINFUNCTYPE(None,
+                                       ctypes.c_void_p, ctypes.c_uint,
+                                       ctypes.c_uint, ctypes.c_ulong)
+        proc = TIMERPROC(post_quit_message)
+        ctypes.windll.user32.SetTimer.argtypes = (ctypes.c_void_p, ctypes.c_uint,
+                                                  ctypes.c_uint, TIMERPROC)
+        ctypes.windll.user32.SetTimer(0, 42, timeout,
+                                      TIMERPROC(post_quit_message))
+        pump_messages()
+
+    def test(self):
+        # Start IE, call .Quit(), and check if the
+        # DWebBrowserEvents2_OnQuit event has fired.  We do this by
+        # calling ShowEvents() and capturing sys.stdout.
+        o = comtypes.client.CreateObject("InternetExplorer.Application")
+        conn = comtypes.client.ShowEvents(o)
+        o.Quit()
+        del o
+
+        self.pump_messages_with_timeout(1000)
 
         stream = sys.stdout
         stream.flush()
