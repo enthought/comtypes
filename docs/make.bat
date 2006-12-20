f5c51c7d688e0c290274d6b08f6765e67dae82dc
thomas.heller <none@none>
2006-12-20 15:02:17 +0000

    New stylesheet for the docs.
    --HG--
    extra : convert_revision : svn%3Aa2f44796-8cc0-49ac-b43f-6a96d556d52d/trunk%4076

--- docs/make.bat
+++ docs/make.bat
@@ -1,13 +1,13 @@
 @echo off
 rem Build the comtypes html pages
 
-set CSS=--stylesheet-path=ctypes.css
-REM set OPTIONS=--time --source-link --initial-header-level=2
-set OPTIONS=--time --source-link
+REM set CSS=--stylesheet-path=ctypes.css
+set CSS=--stylesheet-path=comtypes.css
+set OPTIONS=--time --source-link --initial-header-level=2
+REM set OPTIONS=--time --source-link
 set CMD=c:\python24\python -u c:\python24\scripts\rst2html.py %OPTIONS% %CSS%
 
 %CMD% comtypes.client.txt comtypes.client.html
 %CMD% com_interfaces.txt com_interfaces.html
 
-REM start com_interfaces.html
-REM start comtypes.client.html
+if not "%1" == "" start %1.html
