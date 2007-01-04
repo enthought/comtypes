f5b6c6dad58a05d8e1bd0f5515e39afc2a41347e
thomas.heller <none@none>
2007-01-04 10:11:09 +0000

    Test 2-dimensional safearray.
    --HG--
    extra : convert_revision : svn%3Aa2f44796-8cc0-49ac-b43f-6a96d556d52d/trunk%4095

--- comtypes/test/test_safearray.py
+++ comtypes/test/test_safearray.py
@@ -1,7 +1,7 @@
-import unittest as ut
+import unittest
 from comtypes.automation import VARIANT, VT_ARRAY, VT_VARIANT, VT_I4, VT_R4, VT_R8
 
-class TestCase(ut.TestCase):
+class TestCase(unittest.TestCase):
     def test_1(self):
         v = VARIANT()
         v.value = ((1, 2, 3), ("foo", "bar", None))
@@ -21,6 +21,14 @@ class TestCase(ut.TestCase):
         v = VARIANT(a)
         self.failUnlessEqual(v.vt, VT_ARRAY | VT_R4)
         self.failUnlessEqual(tuple(a.tolist()), v.value)
+
+    def test_2dim_array(self):
+        data = ((1, 2, 3, 4),
+                (5, 6, 7, 8),
+                (9, 10, 11, 12))
+        from comtypes.safearray import SafeArray_FromSequence, UnpackSafeArray
+        a = SafeArray_FromSequence(data)
+        self.failUnlessEqual(UnpackSafeArray(a), data)
 
 if __name__ == "__main__":
     unittest.main()
