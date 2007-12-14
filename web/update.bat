1ca5ae6493362f4edb9cf40b19573608475aa9fa
thomas.heller <none@none>
2007-12-14 09:14:04 +0000

    --HG-- extra : convert_revision : svn%3Aa2f44796-8cc0-49ac-b43f-6a96d556d52d/trunk%40258

--- web/update.bat
+++ web/update.bat
@@ -1,2 +1,2 @@
 call make.bat
-\putty\pscp index.html ctypes.css theller@_starship:/home/crew/theller/public_html/comtypes/
+\putty\pscp index.html scriptcontrol.html ctypes.css theller@_starship:/home/crew/theller/public_html/comtypes/
