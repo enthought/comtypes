065705b8d42024be82f1a1555342b5657c1505a2
thomas.heller <none@none>
2008-04-24 09:48:17 +0000

    Part of this test requires the 'pythoncom' resource.
    --HG--
    extra : convert_revision : svn%3Aa2f44796-8cc0-49ac-b43f-6a96d556d52d/trunk%40295

--- comtypes/test/test_dispinterface.py
+++ comtypes/test/test_dispinterface.py
@@ -1,6 +1,7 @@
 import unittest
 
 from comtypes.server.register import register#, unregister
+from comtypes.test import is_resource_enabled
 
 ################################################################
 import comtypes.test.TestDispServer
@@ -8,77 +9,78 @@ register(comtypes.test.TestDispServer.TestDispServer)
 
 class Test(unittest.TestCase):
 
-    def test_win32com(self):
-        from win32com.client import Dispatch
-        d = Dispatch("TestDispServerLib.TestDispServer")
+    if is_resource_enabled("pythoncom"):
+        def test_win32com(self):
+            from win32com.client import Dispatch
+            d = Dispatch("TestDispServerLib.TestDispServer")
 
-        self.assertEqual(d.eval("3.14"), 3.14)
-        self.assertEqual(d.eval("1 + 2"), 3)
-        self.assertEqual(d.eval("[1 + 2, 'foo', None]"), (3, 'foo', None))
+            self.assertEqual(d.eval("3.14"), 3.14)
+            self.assertEqual(d.eval("1 + 2"), 3)
+            self.assertEqual(d.eval("[1 + 2, 'foo', None]"), (3, 'foo', None))
 
-        self.assertEqual(d.eval2("3.14"), 3.14)
-        self.assertEqual(d.eval2("1 + 2"), 3)
-        self.assertEqual(d.eval2("[1 + 2, 'foo', None]"), (3, 'foo', None))
+            self.assertEqual(d.eval2("3.14"), 3.14)
+            self.assertEqual(d.eval2("1 + 2"), 3)
+            self.assertEqual(d.eval2("[1 + 2, 'foo', None]"), (3, 'foo', None))
 
-        d.eval("__import__('comtypes.client').client.CreateObject('MSScriptControl.ScriptControl')")
+            d.eval("__import__('comtypes.client').client.CreateObject('MSScriptControl.ScriptControl')")
 
-        self.assertEqual(d.EVAL("3.14"), 3.14)
-        self.assertEqual(d.EVAL("1 + 2"), 3)
-        self.assertEqual(d.EVAL("[1 + 2, 'foo', None]"), (3, 'foo', None))
+            self.assertEqual(d.EVAL("3.14"), 3.14)
+            self.assertEqual(d.EVAL("1 + 2"), 3)
+            self.assertEqual(d.EVAL("[1 + 2, 'foo', None]"), (3, 'foo', None))
 
-        self.assertEqual(d.EVAL2("3.14"), 3.14)
-        self.assertEqual(d.EVAL2("1 + 2"), 3)
-        self.assertEqual(d.EVAL2("[1 + 2, 'foo', None]"), (3, 'foo', None))
+            self.assertEqual(d.EVAL2("3.14"), 3.14)
+            self.assertEqual(d.EVAL2("1 + 2"), 3)
+            self.assertEqual(d.EVAL2("[1 + 2, 'foo', None]"), (3, 'foo', None))
 
-        server_id = d.eval("id(self)")
-        self.assertEqual(d.id, server_id)
-        self.assertEqual(d.ID, server_id)
+            server_id = d.eval("id(self)")
+            self.assertEqual(d.id, server_id)
+            self.assertEqual(d.ID, server_id)
 
-        self.assertEqual(d.Name, "spam, spam, spam")
-        self.assertEqual(d.nAME, "spam, spam, spam")
+            self.assertEqual(d.Name, "spam, spam, spam")
+            self.assertEqual(d.nAME, "spam, spam, spam")
 
-        d.SetName("foo bar")
-        self.assertEqual(d.Name, "foo bar")
+            d.SetName("foo bar")
+            self.assertEqual(d.Name, "foo bar")
 
-        # fails
-##        d.name = "blah"
-##        self.assertEqual(d.Name, "blah")
+            # fails
+##            d.name = "blah"
+##            self.assertEqual(d.Name, "blah")
 
-    def test_win32com_dyndispatch(self):
-        from win32com.client.dynamic import Dispatch
-        d = Dispatch("TestDispServerLib.TestDispServer")
+        def test_win32com_dyndispatch(self):
+            from win32com.client.dynamic import Dispatch
+            d = Dispatch("TestDispServerLib.TestDispServer")
 
-        self.assertEqual(d.eval("3.14"), 3.14)
-        self.assertEqual(d.eval("1 + 2"), 3)
-        self.assertEqual(d.eval("[1 + 2, 'foo', None]"), (3, 'foo', None))
+            self.assertEqual(d.eval("3.14"), 3.14)
+            self.assertEqual(d.eval("1 + 2"), 3)
+            self.assertEqual(d.eval("[1 + 2, 'foo', None]"), (3, 'foo', None))
 
-        self.assertEqual(d.eval2("3.14"), 3.14)
-        self.assertEqual(d.eval2("1 + 2"), 3)
-        self.assertEqual(d.eval2("[1 + 2, 'foo', None]"), (3, 'foo', None))
+            self.assertEqual(d.eval2("3.14"), 3.14)
+            self.assertEqual(d.eval2("1 + 2"), 3)
+            self.assertEqual(d.eval2("[1 + 2, 'foo', None]"), (3, 'foo', None))
 
-        d.eval("__import__('comtypes.client').client.CreateObject('MSScriptControl.ScriptControl')")
+            d.eval("__import__('comtypes.client').client.CreateObject('MSScriptControl.ScriptControl')")
 
-        self.assertEqual(d.EVAL("3.14"), 3.14)
-        self.assertEqual(d.EVAL("1 + 2"), 3)
-        self.assertEqual(d.EVAL("[1 + 2, 'foo', None]"), (3, 'foo', None))
+            self.assertEqual(d.EVAL("3.14"), 3.14)
+            self.assertEqual(d.EVAL("1 + 2"), 3)
+            self.assertEqual(d.EVAL("[1 + 2, 'foo', None]"), (3, 'foo', None))
 
-        self.assertEqual(d.EVAL2("3.14"), 3.14)
-        self.assertEqual(d.EVAL2("1 + 2"), 3)
-        self.assertEqual(d.EVAL2("[1 + 2, 'foo', None]"), (3, 'foo', None))
+            self.assertEqual(d.EVAL2("3.14"), 3.14)
+            self.assertEqual(d.EVAL2("1 + 2"), 3)
+            self.assertEqual(d.EVAL2("[1 + 2, 'foo', None]"), (3, 'foo', None))
 
-        server_id = d.eval("id(self)")
-        self.assertEqual(d.id, server_id)
-        self.assertEqual(d.ID, server_id)
+            server_id = d.eval("id(self)")
+            self.assertEqual(d.id, server_id)
+            self.assertEqual(d.ID, server_id)
 
-        self.assertEqual(d.Name, "spam, spam, spam")
-        self.assertEqual(d.nAME, "spam, spam, spam")
+            self.assertEqual(d.Name, "spam, spam, spam")
+            self.assertEqual(d.nAME, "spam, spam, spam")
 
-        d.SetName("foo bar")
-        self.assertEqual(d.Name, "foo bar")
+            d.SetName("foo bar")
+            self.assertEqual(d.Name, "foo bar")
 
-        # fails
-##        d.name = "blah"
-##        self.assertEqual(d.Name, "blah")
+            # fails
+##            d.name = "blah"
+##            self.assertEqual(d.Name, "blah")
 
     def test_comtypes(self):
         from comtypes.client import CreateObject
