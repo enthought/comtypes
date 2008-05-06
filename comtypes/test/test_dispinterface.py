bc2aef24d216725b34dad9cec6776c9d28d32968
thomas.heller <none@none>
2008-05-06 09:43:40 +0000

    Use EnsureDispatch for the early binding win32com test. Only late binding is case insensitive in win32com. Add a jscript test for TestDispComServer.
    --HG--
    extra : convert_revision : svn%3Aa2f44796-8cc0-49ac-b43f-6a96d556d52d/trunk%40318

--- comtypes/test/test_dispinterface.py
+++ comtypes/test/test_dispinterface.py
@@ -11,8 +11,9 @@ class Test(unittest.TestCase):
 
     if is_resource_enabled("pythoncom"):
         def test_win32com(self):
-            from win32com.client import Dispatch
-            d = Dispatch("TestDispServerLib.TestDispServer")
+            # EnsureDispatch is case-sensitive
+            from win32com.client.gencache import EnsureDispatch
+            d = EnsureDispatch("TestDispServerLib.TestDispServer")
 
             self.assertEqual(d.eval("3.14"), 3.14)
             self.assertEqual(d.eval("1 + 2"), 3)
@@ -24,29 +25,19 @@ class Test(unittest.TestCase):
 
             d.eval("__import__('comtypes.client').client.CreateObject('MSScriptControl.ScriptControl')")
 
-            self.assertEqual(d.EVAL("3.14"), 3.14)
-            self.assertEqual(d.EVAL("1 + 2"), 3)
-            self.assertEqual(d.EVAL("[1 + 2, 'foo', None]"), (3, 'foo', None))
-
-            self.assertEqual(d.EVAL2("3.14"), 3.14)
-            self.assertEqual(d.EVAL2("1 + 2"), 3)
-            self.assertEqual(d.EVAL2("[1 + 2, 'foo', None]"), (3, 'foo', None))
-
             server_id = d.eval("id(self)")
             self.assertEqual(d.id, server_id)
-            self.assertEqual(d.ID, server_id)
 
-            self.assertEqual(d.Name, "spam, spam, spam")
-            self.assertEqual(d.nAME, "spam, spam, spam")
+            self.assertEqual(d.name, "spam, spam, spam")
 
             d.SetName("foo bar")
-            self.assertEqual(d.Name, "foo bar")
+            self.assertEqual(d.name, "foo bar")
 
-            # fails
-##            d.name = "blah"
-##            self.assertEqual(d.Name, "blah")
+            d.name = "blah"
+            self.assertEqual(d.name, "blah")
 
         def test_win32com_dyndispatch(self):
+            # dynamic Dispatch is case-IN-sensitive
             from win32com.client.dynamic import Dispatch
             d = Dispatch("TestDispServerLib.TestDispServer")
 
@@ -78,7 +69,7 @@ class Test(unittest.TestCase):
             d.SetName("foo bar")
             self.assertEqual(d.Name, "foo bar")
 
-            # fails
+            # fails.  Why?
 ##            d.name = "blah"
 ##            self.assertEqual(d.Name, "blah")
 
@@ -114,9 +105,14 @@ class Test(unittest.TestCase):
         d.SetName("foo bar")
         self.assertEqual(d.Name, "foo bar")
 
-        # fails
-##        d.name = "blah"
-##        self.assertEqual(d.Name, "blah")
+        d.name = "blah"
+        self.assertEqual(d.Name, "blah")
+
+    def test_withjscript(self):
+        import os
+        jscript = os.path.join(os.path.dirname(__file__), "test_jscript.js")
+        errcode = os.system("cscript -nologo %s" % jscript)
+        self.failUnlessEqual(errcode, 0)
 
 if __name__ == "__main__":
     unittest.main()
