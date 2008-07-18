9c9fd771ea7f3cb27014eefac2935f28441f6ada
thomas.heller <none@none>
2008-07-18 14:33:33 +0000

    Define a main() function.
    --HG--
    extra : convert_revision : svn%3Aa2f44796-8cc0-49ac-b43f-6a96d556d52d/trunk%40350

--- comtypes/test/runtests.py
+++ comtypes/test/runtests.py
@@ -1,5 +1,8 @@
 import sys
 import comtypes.test
 
+def main():
+    sys.exit(comtypes.test.run(sys.argv[1:]))
+
 if __name__ == "__main__":
-    sys.exit(comtypes.test.main(comtypes.test))
+    main()
