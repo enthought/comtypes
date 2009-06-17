6599836f03135d2e291a759b22de418c4c466e95
theller <none@none>
2009-06-17 19:19:36 +0000

    Fix silly NameError.
    --HG--
    extra : convert_revision : svn%3Aa2f44796-8cc0-49ac-b43f-6a96d556d52d/trunk%40527

--- comtypes/test/test_QueryService.py
+++ comtypes/test/test_QueryService.py
@@ -10,7 +10,7 @@ class TestCase(unittest.TestCase):
 
         ie = comtypes.client.CreateObject('InternetExplorer.application')
         ie.navigate2("about:blank",0)
-        sp = IEObj.Document.Body.QueryInterface(comtypes.IServiceProvider)
+        sp = ie.Document.Body.QueryInterface(comtypes.IServiceProvider)
         pacc = sp.QueryService(IAccessible._iid_, IAccessible)
         ie.Quit()
         self.failUnlessEqual(type(pacc), POINTER(IAccessible))
