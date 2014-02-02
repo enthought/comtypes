b82a2e56a53d17ba837e546f9f8a8e621d65b035
Jason R. Coombs <jaraco@jaraco.com>
2014-02-02 12:43:28 -0500

    Declare support for Python 2.7

--- setup.py
+++ setup.py
@@ -101,6 +101,7 @@ classifiers = [
     'Operating System :: Microsoft :: Windows :: Windows CE',
     'Programming Language :: Python',
     'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
     'Topic :: Software Development :: Libraries :: Python Modules',
     ]
