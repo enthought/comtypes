2a283847b0d0b9543f5d5f17f7e84d7e7dd1b514
Christopher L. Farrow <cfarrow@enthought.com>
2014-02-25 07:45:17 -0600

    test commit

--- preview.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM
-REM Convert setup.py's long description to HTML and show it.
-REM
-py25 setup.py --long-description | py25 web\rst2html.py --link-stylesheet --stylesheet=http://www.python.org/styles/styles.css > ~pypi.html
-start ~pypi.html
-del ~pypi.html
