434aeb5ab881e31b5c748e1693a709ede293a932
Vasily Ryabov <vasily.v.ryabov@gmail.com>
2018-06-29 20:28:39 +0300

    Fix clear_comtypes_cache.py for Python 3.x.

--- clear_comtypes_cache.py
+++ clear_comtypes_cache.py
@@ -1,61 +1,56 @@
 import os
 import sys
 import shutil
-from ctypes import windll
 
-def is_cache():
+def get_next_cache_dir():
     try:
-        import comtypes.gen
+        print("import comtypes.gen")
+        # change working directory to avoid import from local folder
+        # during installation process
+        os.chdir(os.path.dirname(sys.executable))
+        import comtypes
+        import comtypes.client
+        print("DONE")
+        return comtypes.client._code_cache._find_gen_dir()
     except ImportError:
-        return
-    return comtypes.gen.__path__[0]
+        return None
 
 
 def _remove(directory):
     shutil.rmtree(directory)
-    print("Removed directory %s" % directory)
+    print('Removed directory "%s"' % directory)
 
 
-install_text = """\
-When installing a new comtypes version, it is recommended to remove
-the comtypes\gen directory and the automatically generated modules
-it contains.  This directory and the modules will be regenerated
-on demand.
+def remove_directory(directory, silent):
+    if directory:
+        if silent:
+            _remove(directory)
+        else:
+            try:
+                confirm = raw_input('Remove directory "%s" ? (y/n)' % directory)
+            except NameError:
+                confirm = input('Remove cache directory "%s" ? (y/n)' % directory)
+            if confirm.lower() == 'y':
+                _remove(directory)
+            else:
+                print('Directory "%s" NOT removed' % directory)
+                return False
+    return True
 
-Should the installer delete all the files in this directory?"""
 
-deinstall_text = """\
-The comtypes\gen directory contains modules that comtypes
-automatically generates.
-
-Should this directory be removed?"""
-
-if len(sys.argv) > 1 and "-install" in sys.argv[1:]:
-    title = "Install comtypes"
-    text = install_text
-else:
-    title = "Remove comtypes"
-    text = deinstall_text
-
-if len(sys.argv) > 1 and "-silent" in sys.argv[1:]:
+if len(sys.argv) > 1 and "-y" in sys.argv[1:]:
     silent = True
 else:
     silent = False
 
 
+# First iteration may get folder with restricted rights.
+# Second iteration always gets temp cache folder (writable for all).
+directory = get_next_cache_dir()
+removed = remove_directory(directory, silent)
 
-IDYES = 6
-IDNO = 7
-MB_YESNO = 4
-MB_ICONWARNING = 48
-directory = is_cache()
-if directory:
-    if silent:
-        _remove(directory)
-    else:
-        res = windll.user32.MessageBoxA(0, text, title,
-                MB_YESNO|MB_ICONWARNING)
-        if res == IDYES:
-            _remove(directory)
-        else:
-            print("Directory %s NOT removed" % directory)
+if removed:
+    directory = get_next_cache_dir()
+
+    # do not request confirmation if already confirmed and removed
+    remove_directory(directory, silent=removed)
