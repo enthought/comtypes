6d1e9fbca8a1cb22b73bee0ab70837424400052d
Vasily Ryabov <vasily.v.ryabov@gmail.com>
2018-07-30 18:42:45 +0300

    Re-write pip tests to catch issue #158.

--- test_pip_install.py
+++ test_pip_install.py
@@ -1,7 +1,9 @@
 """This test covers 'pip install' issue #155"""
 import os
 import sys
+import shutil
 import subprocess
+import unittest
 
 def read_version():
     # Determine the version number by reading it from the file
@@ -13,12 +15,32 @@ def read_version():
             return value.strip().strip('"').strip("'")
     raise NotImplementedError("__version__ is not found in __init__.py")
 
-# prepare the same package that is usually uploaded to PyPI
-subprocess.check_call([sys.executable, 'setup.py', 'sdist', '--format=zip'])
 
-filename_for_upload = 'comtypes-%s.zip' % read_version()
-target_package = os.path.join(os.getcwd(), 'dist', filename_for_upload)
+class TestPipInstall(unittest.TestCase):
 
-# run "pip install comtypes-x.y.z.zip"
-pip_exe = os.path.join(os.path.dirname(sys.executable), 'Scripts', 'pip.exe')
-subprocess.check_call([pip_exe, 'install', target_package])
+    @classmethod
+    def setUpClass(cls):
+        print("Calling setUpClass...")
+        # prepare the same package that is usually uploaded to PyPI
+        subprocess.check_call([sys.executable, 'setup.py', 'sdist', '--format=zip'])
+
+        filename_for_upload = 'comtypes-%s.zip' % read_version()
+        cls.target_package = os.path.join(os.getcwd(), 'dist', filename_for_upload)
+        cls.pip_exe = os.path.join(os.path.dirname(sys.executable), 'Scripts', 'pip.exe')
+
+    def test_pip_install(self):
+        """Test that "pip install comtypes-x.y.z.zip" works"""
+        subprocess.check_call([self.pip_exe, 'install', self.target_package])
+
+    def test_no_cache_dir_custom_location(self):
+        """Test that 'pip install comtypes-x.y.z.zip --no-cache-dir --target="...\custom location"' works"""
+        custom_dir = os.path.join(os.getcwd(), 'custom location')
+        if os.path.exists(custom_dir):
+            shutil.rmtree(custom_dir)
+        os.makedirs(custom_dir)
+        # subprocess.check_call([self.pip_exe, 'install', self.target_package, '--no-cache-dir', '--target="{}"'.format(custom_dir)], shell=True)
+        subprocess.check_call('{} install {} --no-cache-dir --target="{}"'.format(self.pip_exe, self.target_package, custom_dir))
+
+
+if __name__ == '__main__':
+    unittest.main()
