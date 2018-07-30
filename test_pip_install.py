60fedbc2cdde68b2624d4979e7e0d7846be0aa7f
Vasily Ryabov <vasily.v.ryabov@gmail.com>
2018-07-30 20:40:13 +0300

    Fix Python 2.6 compatibility.

--- test_pip_install.py
+++ test_pip_install.py
@@ -37,7 +37,7 @@ class TestPipInstall(unittest.TestCase):
             shutil.rmtree(custom_dir)
         os.makedirs(custom_dir)
 
-        subprocess.check_call('{} install {} --no-cache-dir --target="{}"'.format(self.pip_exe, self.target_package, custom_dir))
+        subprocess.check_call('{0} install {1} --no-cache-dir --target="{2}"'.format(self.pip_exe, self.target_package, custom_dir))
 
 
 if __name__ == '__main__':
