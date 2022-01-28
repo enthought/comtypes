409dbcfa4f3f8f3be270802a376bc038cd8d158d
Cosmin Poieana <cosmin@robocorp.com>
2022-01-28 14:38:08 +0000

    Version bump

--- comtypes/__init__.py
+++ comtypes/__init__.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 # comtypes version numbers follow semver (http://semver.org/) and PEP 440
-__version__ = "1.1.10"
+__version__ = "1.1.11"
 
 import logging
 class NullHandler(logging.Handler):
