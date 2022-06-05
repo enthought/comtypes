1bb309c5d53e51e27e7b6787688e94a958d0825e
junkmd <jun.k.job.finding12@gmail.com>
2022-06-05 09:45:14 +0900

    trial for test indicator

---
+++ ci/runTestsuite.ps1
@@ -0,0 +1,58 @@
+# Credits to: 
+# https://github.com/originell/jpype/appveyor/runTestsuite.ps1
+#
+function xslt_transform($xml, $xsl, $output)
+{
+	trap [Exception]
+	{
+	    Write-Host $_.Exception
+	}
+	
+	$xslt = New-Object System.Xml.Xsl.XslCompiledTransform
+	$xslt.Load($xsl)
+	$xslt.Transform($xml, $output)
+}
+
+function upload($file) {
+    trap [Exception]
+    {
+        Write-Host $_.Exception
+    }
+    Write-Host "Uploading: " $file
+
+    $wc = New-Object 'System.Net.WebClient'
+    $wc.UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", $file)
+
+    $test_report_dir = ".\TestResultsReport"
+    md -Force $test_report_dir | Out-Null
+    $revid = $($env:APPVEYOR_REPO_COMMIT).Substring(0, 8)
+    $rep_dest = "$test_report_dir\$($env:APPVEYOR_JOB_ID)-$revid-$($env:PYTHON_VERSION)-$($env:PYTHON_ARCH)-UIA$($env:UIA_SUPPORT)-result.xml"
+
+    Write-Host "Copying test report to: " $rep_dest
+    cp $file $rep_dest
+    Push-AppveyorArtifact $rep_dest
+}
+
+function run {
+    Write-Host $env:APPVEYOR_BUILD_FOLDER
+
+    cd $env:APPVEYOR_BUILD_FOLDER
+    $stylesheet =  "./ci/transform_xunit_to_appveyor.xsl"
+    $input = "unittest.xml"
+    $output = "transformed.xml"
+
+    python -m unittest discover -s ./comtypes/test -t comtypes\test > unittest.xml
+    $success = $?
+    Write-Host "result code of unittest:" $success
+
+    xslt_transform $input $stylesheet $output
+
+    upload $output
+
+    # return exit code of testsuite
+    if ( -not $success) {
+        throw "testsuite not successful"
+    }
+}
+
+run
