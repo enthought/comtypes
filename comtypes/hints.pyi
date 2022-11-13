db0931d98ab977e2e3eef98133826a7a0ff5c93e
junkmd <45822440+junkmd@users.noreply.github.com>
2022-11-13 23:59:38 +0900

    Add type annotations to `comtypes.automation` (#374)
    * add type annotations
    
    * improve `VARIANT.__repr__`

--- comtypes/hints.pyi
+++ comtypes/hints.pyi
@@ -4,7 +4,9 @@ from typing import (
 )
 
 # symbols those what might occur recursive imports in runtime.
+from comtypes.automation import IDispatch as IDispatch, VARIANT as VARIANT
 from comtypes.server import IClassFactory as IClassFactory
+from comtypes.typeinfo import ITypeInfo as ITypeInfo
 
 
 def AnnoField() -> Any:
