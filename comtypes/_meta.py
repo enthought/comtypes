2d21a3a441adfb81c8e94d74490296dc662359bf
Kevin Schlosser <kdschlosser@users.noreply.github.com>
2022-12-06 05:01:34 -0700

    pep8

--- comtypes/_meta.py
+++ comtypes/_meta.py
@@ -2,6 +2,7 @@
 from ctypes import POINTER, c_void_p, cast
 import comtypes
 
+
 ################################################################
 # metaclass for CoClass (in comtypes/__init__.py)
 
@@ -10,14 +11,16 @@ def _wrap_coclass(self):
     # but we really want this interface:
     itf = self._com_interfaces_[0]
     punk = cast(self, POINTER(itf))
-    result = punk.QueryInterface(itf)
+    result = punk.QueryInterface(itf)  # NOQA
     result.__dict__["__clsid"] = str(self._reg_clsid_)
     return result
+
 
 def _coclass_from_param(cls, obj):
     if isinstance(obj, (cls._com_interfaces_[0], cls)):
         return obj
     raise TypeError(obj)
+
 
 #
 # The mro() of a POINTER(App) type, where class App is a subclass of CoClass:
@@ -38,23 +41,28 @@ class _coclass_meta(type):
     # POINTER(...) type gets a __ctypes_from_outparam__ method which
     # will QueryInterface for the default interface: the first one on
     # the coclass' _com_interfaces_ list.
-    def __new__(cls, name, bases, namespace):
-        klass = type.__new__(cls, name, bases, namespace)
+    def __new__(mcs, name, bases, namespace):
+        klass = type.__new__(mcs, name, bases, namespace)
         if bases == (object,):
             return klass
         # XXX We should insist that a _reg_clsid_ is present.
         if "_reg_clsid_" in namespace:
             clsid = namespace["_reg_clsid_"]
             comtypes.com_coclass_registry[str(clsid)] = klass
-        PTR = _coclass_pointer_meta("POINTER(%s)" % klass.__name__,
-                                    (klass, c_void_p),
-                                    {"__ctypes_from_outparam__": _wrap_coclass,
-                                     "from_param": classmethod(_coclass_from_param),
-                                     })
-        from ctypes import _pointer_type_cache
+        PTR = _coclass_pointer_meta(
+            "POINTER(%s)" % klass.__name__,
+            (klass, c_void_p),
+            {
+                "__ctypes_from_outparam__": _wrap_coclass,
+                "from_param": classmethod(_coclass_from_param),
+            }
+        )
+
+        from ctypes import _pointer_type_cache  # NOQA
         _pointer_type_cache[klass] = PTR
 
         return klass
+
 
 # will not work if we change the order of the two base classes!
 class _coclass_pointer_meta(type(c_void_p), _coclass_meta):
