11accfe86753bc2c071cb7dca2b0908ceb399e9b
Kevin Schlosser <kdschlosser@users.noreply.github.com>
2022-12-06 06:05:36 -0700

    pep8

--- comtypes/util.py
+++ comtypes/util.py
@@ -1,7 +1,9 @@
-"""This module defines the funtions byref_at(cobj, offset)
+"""
+This module defines the funtions byref_at(cobj, offset)
 and cast_field(struct, fieldname, fieldtype).
 """
 from ctypes import *
+
 
 def _calc_offset():
     # Internal helper function that calculates where the object
@@ -11,30 +13,34 @@ def _calc_offset():
     # object that a byref() call returns):
     class PyCArgObject(Structure):
         class value(Union):
-            _fields_ = [("c", c_char),
-                        ("h", c_short),
-                        ("i", c_int),
-                        ("l", c_long),
-                        ("q", c_longlong),
-                        ("d", c_double),
-                        ("f", c_float),
-                        ("p", c_void_p)]
+            _fields_ = [
+                ("c", c_char),
+                ("h", c_short),
+                ("i", c_int),
+                ("l", c_long),
+                ("q", c_longlong),
+                ("d", c_double),
+                ("f", c_float),
+                ("p", c_void_p)
+            ]
         #
         # Thanks to Lenard Lindstrom for this tip:
         # sizeof(PyObject_HEAD) is the same as object.__basicsize__.
         #
-        _fields_ = [("PyObject_HEAD", c_byte * object.__basicsize__),
-                    ("pffi_type", c_void_p),
-                    ("tag", c_char),
-                    ("value", value),
-                    ("obj", c_void_p),
-                    ("size", c_int)]
+        _fields_ = [
+            ("PyObject_HEAD", c_byte * object.__basicsize__),
+            ("pffi_type", c_void_p),
+            ("tag", c_char),
+            ("value", value),
+            ("obj", c_void_p),
+            ("size", c_int)
+        ]
 
         _anonymous_ = ["value"]
 
     # additional checks to make sure that everything works as expected
 
-    if sizeof(PyCArgObject) != type(byref(c_int())).__basicsize__:
+    if sizeof(PyCArgObject) != type(byref(c_int())).__basicsize__:  # NOQA
         raise RuntimeError("sizeof(PyCArgObject) invalid")
 
     obj = c_int()
@@ -42,23 +48,29 @@ def _calc_offset():
 
     argobj = PyCArgObject.from_address(id(ref))
 
-    if argobj.obj != id(obj) or \
-       argobj.p != addressof(obj) or \
-       argobj.tag != 'P':
+    if (
+        argobj.obj != id(obj) or
+        argobj.p != addressof(obj) or
+        argobj.tag != 'P'
+    ):
         raise RuntimeError("PyCArgObject field definitions incorrect")
 
-    return PyCArgObject.p.offset # offset of the pointer field
+    return PyCArgObject.p.offset  # offset of the pointer field
+
 
 ################################################################
 #
 # byref_at
 #
-def byref_at(obj, offset,
-             _byref=byref,
-             _c_void_p_from_address = c_void_p.from_address,
-             _byref_pointer_offset = _calc_offset()
-             ):
-    """byref_at(cobj, offset) behaves similar this C code:
+def byref_at(
+    obj,
+    offset,
+    _byref=byref,
+    _c_void_p_from_address=c_void_p.from_address,
+    _byref_pointer_offset=_calc_offset()
+):
+    """
+    byref_at(cobj, offset) behaves similar this C code:
 
         (((char *)&obj) + offset)
 
@@ -68,8 +80,9 @@ def byref_at(obj, offset,
     ref = _byref(obj)
     # Change the pointer field in the created byref object by adding
     # 'offset' to it:
-    _c_void_p_from_address(id(ref)
-                           + _byref_pointer_offset).value += offset
+    _c_void_p_from_address(
+        id(ref) + _byref_pointer_offset
+    ).value += offset
     return ref
 
 
@@ -77,20 +90,27 @@ def byref_at(obj, offset,
 #
 # cast_field
 #
-def cast_field(struct, fieldname, fieldtype, offset=0,
-               _POINTER=POINTER,
-               _byref_at=byref_at,
-               _byref=byref,
-               _divmod=divmod,
-               _sizeof=sizeof,
-               ):
+def cast_field(
+    struct,
+    fieldname,
+    fieldtype,
+    offset=0,  # NOQA
+    _POINTER=POINTER,
+    _byref_at=byref_at,
+    _byref=byref,
+    _divmod=divmod,
+    _sizeof=sizeof,
+):
     """cast_field(struct, fieldname, fieldtype)
 
     Return the contents of a struct field as it it were of type
     'fieldtype'.
     """
     fieldoffset = getattr(type(struct), fieldname).offset
-    return cast(_byref_at(struct, fieldoffset),
-                _POINTER(fieldtype))[0]
+    return cast(
+        _byref_at(struct, fieldoffset),
+        _POINTER(fieldtype)
+    )[0]
+
 
 __all__ = ["byref_at", "cast_field"]
