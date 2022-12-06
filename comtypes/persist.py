5e467c5c3527b0418683a949dc7d5e8e394b7357
Kevin Schlosser <kdschlosser@users.noreply.github.com>
2022-12-06 05:35:09 -0700

    pep8

--- comtypes/persist.py
+++ comtypes/persist.py
@@ -11,53 +11,69 @@ interface, useful in client code.
 """
 from ctypes import *
 from ctypes.wintypes import WORD, DWORD, BOOL
-from comtypes import GUID, IUnknown, COMMETHOD, HRESULT, dispid
+from comtypes import GUID, IUnknown, COMMETHOD, HRESULT
 from comtypes import IPersist
 from comtypes.automation import VARIANT, tagEXCEPINFO
 
+
 # XXX Replace by canonical solution!!!
 WSTRING = c_wchar_p
+
 
 class IErrorLog(IUnknown):
     _iid_ = GUID('{3127CA40-446E-11CE-8135-00AA004BB851}')
     _idlflags_ = []
     _methods_ = [
-        COMMETHOD([], HRESULT, 'AddError',
-                  ( ['in'], WSTRING, 'pszPropName' ),
-                  ( ['in'], POINTER(tagEXCEPINFO), 'pExcepInfo' )),
-        ]
+        COMMETHOD(
+            [], HRESULT, 'AddError',
+            (['in'], WSTRING, 'pszPropName'),
+            (['in'], POINTER(tagEXCEPINFO), 'pExcepInfo')
+        )
+    ]
+
 
 class IPropertyBag(IUnknown):
     _iid_ = GUID('{55272A00-42CB-11CE-8135-00AA004BB851}')
     _idlflags_ = []
     _methods_ = [
-        # XXX Note: According to MSDN, pVar and pErrorLog are ['in', 'out'] parameters.
+        # XXX Note: According to MSDN, pVar and pErrorLog
+        # are ['in', 'out'] parameters.
         #
         # XXX ctypes does NOT yet accept POINTER(IErrorLog) as 'out' parameter:
-        # TypeError: 'out' parameter 3 must be a pointer type, not POINTER(IErrorLog)
-        COMMETHOD([], HRESULT, 'Read',
-                  ( ['in'], WSTRING, 'pszPropName' ),
-                  ( ['in', 'out'], POINTER(VARIANT), 'pVar' ),
-                  ( ['in'], POINTER(IErrorLog), 'pErrorLog' )),
-##                  ( ['in', 'out'], POINTER(IErrorLog), 'pErrorLog' )),
-        COMMETHOD([], HRESULT, 'Write',
-                  ( ['in'], WSTRING, 'pszPropName' ),
-                  ( ['in'], POINTER(VARIANT), 'pVar' )),
-        ]
+        # TypeError: 'out' parameter 3 must be a pointer
+        # type, not POINTER(IErrorLog)
+        COMMETHOD(
+            [], HRESULT, 'Read',
+            (['in'], WSTRING, 'pszPropName'),
+            (['in', 'out'], POINTER(VARIANT), 'pVar'),
+            (['in'], POINTER(IErrorLog), 'pErrorLog')
+            # ( ['in', 'out'], POINTER(IErrorLog), 'pErrorLog' )
+        ),
+        COMMETHOD(
+            [], HRESULT, 'Write',
+            (['in'], WSTRING, 'pszPropName'),
+            (['in'], POINTER(VARIANT), 'pVar')
+        )
+    ]
+
 
 class IPersistPropertyBag(IPersist):
     _iid_ = GUID('{37D84F60-42CB-11CE-8135-00AA004BB851}')
     _idlflags_ = []
     _methods_ = [
         COMMETHOD([], HRESULT, 'InitNew'),
-        COMMETHOD([], HRESULT, 'Load',
-                  ( ['in'], POINTER(IPropertyBag), 'pPropBag' ),
-                  ( ['in'], POINTER(IErrorLog), 'pErrorLog' )),
-        COMMETHOD([], HRESULT, 'Save',
-                  ( ['in'], POINTER(IPropertyBag), 'pPropBag' ),
-                  ( ['in'], c_int, 'fClearDirty' ),
-                  ( ['in'], c_int, 'fSaveAllProperties' )),
-        ]
+        COMMETHOD(
+            [], HRESULT, 'Load',
+            (['in'], POINTER(IPropertyBag), 'pPropBag'),
+            (['in'], POINTER(IErrorLog), 'pErrorLog')
+        ),
+        COMMETHOD(
+            [], HRESULT, 'Save',
+            (['in'], POINTER(IPropertyBag), 'pPropBag'),
+            (['in'], c_int, 'fClearDirty'),
+            (['in'], c_int, 'fSaveAllProperties')
+        )
+    ]
 
 
 CLIPFORMAT = WORD
@@ -70,6 +86,7 @@ PROPBAG2_TYPE_STREAM = 4
 PROPBAG2_TYPE_STORAGE = 5
 PROPBAG2_TYPE_MONIKER = 6
 
+
 class tagPROPBAG2(Structure):
     _fields_ = [
         ('dwType', c_ulong),
@@ -78,50 +95,66 @@ class tagPROPBAG2(Structure):
         ('dwHint', c_ulong),
         ('pstrName', WSTRING),
         ('clsid', GUID),
-        ]
+    ]
+
 
 class IPropertyBag2(IUnknown):
     _iid_ = GUID('{22F55882-280B-11D0-A8A9-00A0C90C2004}')
     _idlflags_ = []
     _methods_ = [
-        COMMETHOD([], HRESULT, 'Read',
-                  ( ['in'], c_ulong, 'cProperties' ),
-                  ( ['in'], POINTER(tagPROPBAG2), 'pPropBag' ),
-                  ( ['in'], POINTER(IErrorLog), 'pErrLog' ),
-                  ( ['out'], POINTER(VARIANT), 'pvarValue' ),
-                  ( ['out'], POINTER(HRESULT), 'phrError' )),
-        COMMETHOD([], HRESULT, 'Write',
-                  ( ['in'], c_ulong, 'cProperties' ),
-                  ( ['in'], POINTER(tagPROPBAG2), 'pPropBag' ),
-                  ( ['in'], POINTER(VARIANT), 'pvarValue' )),
-        COMMETHOD([], HRESULT, 'CountProperties',
-                  ( ['out'], POINTER(c_ulong), 'pcProperties' )),
-        COMMETHOD([], HRESULT, 'GetPropertyInfo',
-                  ( ['in'], c_ulong, 'iProperty' ),
-                  ( ['in'], c_ulong, 'cProperties' ),
-                  ( ['out'], POINTER(tagPROPBAG2), 'pPropBag' ),
-                  ( ['out'], POINTER(c_ulong), 'pcProperties' )),
-        COMMETHOD([], HRESULT, 'LoadObject',
-                  ( ['in'], WSTRING, 'pstrName' ),
-                  ( ['in'], c_ulong, 'dwHint' ),
-                  ( ['in'], POINTER(IUnknown), 'punkObject' ),
-                  ( ['in'], POINTER(IErrorLog), 'pErrLog' )),
-        ]
+        COMMETHOD(
+            [], HRESULT, 'Read',
+            (['in'], c_ulong, 'cProperties'),
+            (['in'], POINTER(tagPROPBAG2), 'pPropBag'),
+            (['in'], POINTER(IErrorLog), 'pErrLog'),
+            (['out'], POINTER(VARIANT), 'pvarValue'),
+            (['out'], POINTER(HRESULT), 'phrError')
+        ),
+        COMMETHOD(
+            [], HRESULT, 'Write',
+            (['in'], c_ulong, 'cProperties'),
+            (['in'], POINTER(tagPROPBAG2), 'pPropBag'),
+            (['in'], POINTER(VARIANT), 'pvarValue')
+        ),
+        COMMETHOD(
+            [], HRESULT, 'CountProperties',
+            (['out'], POINTER(c_ulong), 'pcProperties')
+        ),
+        COMMETHOD(
+            [], HRESULT, 'GetPropertyInfo',
+            (['in'], c_ulong, 'iProperty'),
+            (['in'], c_ulong, 'cProperties'),
+            (['out'], POINTER(tagPROPBAG2), 'pPropBag'),
+            (['out'], POINTER(c_ulong), 'pcProperties')
+        ),
+        COMMETHOD(
+            [], HRESULT, 'LoadObject',
+            (['in'], WSTRING, 'pstrName'),
+            (['in'], c_ulong, 'dwHint'),
+            (['in'], POINTER(IUnknown), 'punkObject'),
+            (['in'], POINTER(IErrorLog), 'pErrLog')
+        )
+    ]
+
 
 class IPersistPropertyBag2(IPersist):
     _iid_ = GUID('{22F55881-280B-11D0-A8A9-00A0C90C2004}')
     _idlflags_ = []
     _methods_ = [
         COMMETHOD([], HRESULT, 'InitNew'),
-        COMMETHOD([], HRESULT, 'Load',
-                  ( ['in'], POINTER(IPropertyBag2), 'pPropBag' ),
-                  ( ['in'], POINTER(IErrorLog), 'pErrLog' )),
-        COMMETHOD([], HRESULT, 'Save',
-                  ( ['in'], POINTER(IPropertyBag2), 'pPropBag' ),
-                  ( ['in'], c_int, 'fClearDirty' ),
-                  ( ['in'], c_int, 'fSaveAllProperties' )),
-        COMMETHOD([], HRESULT, 'IsDirty'),
-        ]
+        COMMETHOD(
+            [], HRESULT, 'Load',
+            (['in'], POINTER(IPropertyBag2), 'pPropBag'),
+            (['in'], POINTER(IErrorLog), 'pErrLog')
+        ),
+        COMMETHOD(
+            [], HRESULT, 'Save',
+            (['in'], POINTER(IPropertyBag2), 'pPropBag'),
+            (['in'], c_int, 'fClearDirty'),
+            (['in'], c_int, 'fSaveAllProperties')
+        ),
+        COMMETHOD([], HRESULT, 'IsDirty')
+    ]
 
 
 # STGM constants
@@ -159,28 +192,40 @@ STGM_DELETEONRELEASE = 0x04000000
 
 LPOLESTR = LPCOLESTR = c_wchar_p
 
+
 class IPersistFile(IPersist):
     _iid_ = GUID('{0000010B-0000-0000-C000-000000000046}')
     _idlflags_ = []
     _methods_ = [
         COMMETHOD([], HRESULT, 'IsDirty'),
-        COMMETHOD([], HRESULT, 'Load',
-                  ( ['in'], LPCOLESTR, 'pszFileName' ),
-                  ( ['in'], DWORD, 'dwMode' )),
-        COMMETHOD([], HRESULT, 'Save',
-                  ( ['in'], LPCOLESTR, 'pszFileName' ),
-                  ( ['in'], BOOL, 'fRemember' )),
-        COMMETHOD([], HRESULT, 'SaveCompleted',
-                  ( ['in'], LPCOLESTR, 'pszFileName' )),
-        COMMETHOD([], HRESULT, 'GetCurFile',
-                  ( ['out'], POINTER(LPOLESTR), 'ppszFileName' ))
-        ]
+        COMMETHOD(
+            [], HRESULT, 'Load',
+            (['in'], LPCOLESTR, 'pszFileName'),
+            (['in'], DWORD, 'dwMode')
+        ),
+        COMMETHOD(
+            [], HRESULT, 'Save',
+            (['in'], LPCOLESTR, 'pszFileName'),
+            (['in'], BOOL, 'fRemember')
+        ),
+        COMMETHOD(
+            [], HRESULT, 'SaveCompleted',
+            (['in'], LPCOLESTR, 'pszFileName')
+        ),
+        COMMETHOD(
+            [], HRESULT, 'GetCurFile',
+            (['out'], POINTER(LPOLESTR), 'ppszFileName')
+        )
+    ]
 
 
-from comtypes import COMObject
-from comtypes.hresult import *
+from comtypes import COMObject  # NOQA
+from comtypes.hresult import *  # NOQA
+
+
 class DictPropertyBag(COMObject):
-    """An object implementing the IProperty interface on a dictionary.
+    """
+    An object implementing the IProperty interface on a dictionary.
 
     Pass named values in the constructor for the client to Read(), or
     retrieve from the .values instance variable after the client has
@@ -192,7 +237,7 @@ class DictPropertyBag(COMObject):
         super(DictPropertyBag, self).__init__()
         self.values = kw
 
-    def Read(self, this, name, pVar, errorlog):
+    def Read(self, _, name, pVar, __):
         try:
             val = self.values[name]
         except KeyError:
@@ -206,7 +251,7 @@ class DictPropertyBag(COMObject):
             var.ChangeType(typecode)
         return S_OK
 
-    def Write(self, this, name, var):
+    def Write(self, _, name, var):
         val = var[0].value
         self.values[name] = val
         return S_OK
