ea0bc918294ed88cf7917f5a3a46b754acd2c4f4
CristiFati <fati_utcluj@yahoo.com>
2022-12-13 14:34:41 +0200

    Force Python >=3.7 and update classifiers

--- setup.py
+++ setup.py
@@ -88,9 +88,12 @@ classifiers = [
     'Intended Audience :: Developers',
     'License :: OSI Approved :: MIT License',
     'Operating System :: Microsoft :: Windows',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
     'Topic :: Software Development :: Libraries :: Python Modules',
     ]
 
@@ -182,6 +185,7 @@ setup_params = dict(
     ],
 
     platforms=["Windows"],
+    python_requires=">=3.7",
 )
 
 if __name__ == '__main__':
