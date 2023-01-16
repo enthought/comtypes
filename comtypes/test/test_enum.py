0aa863d733f9aa590a5fa887822f26d5a7a288b9
jonschz <jonschz@users.noreply.github.com>
2023-01-16 09:17:39 +0100

    change enum_test to a non-empty IEnum

--- comtypes/test/test_enum.py
+++ comtypes/test/test_enum.py
@@ -1,3 +1,4 @@
+import contextlib
 import unittest as ut
 
 import comtypes
@@ -6,20 +7,16 @@ import comtypes.client
 
 class Test_Enum(ut.TestCase):
     def test_enum(self):
-        comtypes.client.GetModule("msvidctl.dll")
-        comtypes.client.GetModule("quartz.dll")
+        with contextlib.redirect_stdout(None):  # supress warnings, see test_client.py
+            comtypes.client.GetModule("msvidctl.dll")
         from comtypes.gen import MSVidCtlLib as vidlib
-        from comtypes.gen import QuartzTypeLib as quartz
 
-        # FilgraphManager has the same CLSID as FilterGraph
-        filtergraph = comtypes.CoCreateInstance(
-            quartz.FilgraphManager._reg_clsid_,
-            vidlib.IGraphBuilder,
-            comtypes.CLSCTX_INPROC_SERVER,
+        avisplitter = comtypes.client.CreateObject(
+            "{1b544c20-fd0b-11ce-8c63-00aa0044b51e}",  # CLSID_AviSplitter
+            interface=vidlib.IBaseFilter,
         )
-        enum_filters = filtergraph.EnumFilters()
-        # make sure enum_filters is iterable
-        [_ for _ in enum_filters]
+        pins = list(avisplitter.EnumPins())
+        self.assertGreater(len(pins), 0)
 
 
 if __name__ == "__main__":
