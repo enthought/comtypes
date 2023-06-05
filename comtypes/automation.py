ae3f6e99e3b5cec992cc0d5c468999b7b6af39a4
Jun Komoda <45822440+junkmd@users.noreply.github.com>
2023-06-06 07:41:00 +0900

    split `DISPPARAMS` instantiation in `IDispatch.Invoke` method with `__make_dp` method (#485)
    (cherry picked from commit 62ce3035b44ae8e7c957b8cd8dca42bfa23b9dd3)

--- comtypes/automation.py
+++ comtypes/automation.py
@@ -843,6 +843,20 @@ class IDispatch(IUnknown):
         self.__com_Invoke(memid, riid_null, lcid, invkind, dp, var, None, argerr)
         return var._get_value(dynamic=True)
 
+    def __make_dp(self, _invkind: int, *args: Any) -> DISPPARAMS:
+        array = (VARIANT * len(args))()
+        for i, a in enumerate(args[::-1]):
+            array[i].value = a
+        dp = DISPPARAMS()
+        dp.cArgs = len(args)
+        dp.rgvarg = array
+        if _invkind in (DISPATCH_PROPERTYPUT, DISPATCH_PROPERTYPUTREF):  # propput
+            dp.cNamedArgs = 1
+            dp.rgdispidNamedArgs = pointer(DISPID(DISPID_PROPERTYPUT))
+        else:
+            dp.cNamedArgs = 0
+        return dp
+
     def Invoke(self, dispid: int, *args: Any, **kw: Any) -> Any:
         """Invoke a method or property."""
 
@@ -857,33 +871,10 @@ class IDispatch(IUnknown):
         _lcid = kw.pop("_lcid", 0)
         if kw:
             raise ValueError("named parameters not yet implemented")
-
+        dp = self.__make_dp(_invkind, *args)
         result = VARIANT()
         excepinfo = EXCEPINFO()
         argerr = c_uint()
-
-        if _invkind in (DISPATCH_PROPERTYPUT, DISPATCH_PROPERTYPUTREF):  # propput
-            array = (VARIANT * len(args))()
-
-            for i, a in enumerate(args[::-1]):
-                array[i].value = a
-
-            dp = DISPPARAMS()
-            dp.cArgs = len(args)
-            dp.cNamedArgs = 1
-            dp.rgvarg = array
-            dp.rgdispidNamedArgs = pointer(DISPID(DISPID_PROPERTYPUT))
-        else:
-            array = (VARIANT * len(args))()
-
-            for i, a in enumerate(args[::-1]):
-                array[i].value = a
-
-            dp = DISPPARAMS()
-            dp.cArgs = len(args)
-            dp.cNamedArgs = 0
-            dp.rgvarg = array
-
         try:
             self.__com_Invoke(
                 dispid,
