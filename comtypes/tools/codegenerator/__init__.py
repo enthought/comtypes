e9e620a731e7e16a03c66c1bed001a9bfb7a71be
Jun Komoda <45822440+junkmd@users.noreply.github.com>
2024-05-22 08:58:04 +0900

    `tools.codegenerator` subdirectory package part 1 (#546)
    * add `codegenerator` package
    - split `tools.codegenerator` into `codegenerator` and `helpers`
    - add `__init__.py`
    
    * import stuffs from `helpers` into `codegenerator`

---
+++ comtypes/tools/codegenerator/__init__.py
@@ -0,0 +1,5 @@
+from comtypes.tools.codegenerator.helpers import (  # noqa
+    name_friendly_module,
+    name_wrapper_module,
+)
+from comtypes.tools.codegenerator.codegenerator import CodeGenerator, version  # noqa
