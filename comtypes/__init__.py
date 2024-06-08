40ab9641ac1cc58e2d6932df1bbff9bdd69ad7e3
junkmd <jun.k.job.finding12@gmail.com>
2024-06-08 22:27:58 +0900

    import `BSTR` from `_bstr`

--- comtypes/__init__.py
+++ comtypes/__init__.py
@@ -51,6 +51,7 @@ from comtypes._memberspec import (
     _resolve_argspec,
 )
 from comtypes._tlib_version_checker import _check_version  # noqa
+from comtypes._bstr import BSTR  # noqa
 
 
 _all_slice = slice(None, None, None)
@@ -648,38 +649,6 @@ class _compointer_base(c_void_p, metaclass=_compointer_meta):
             except KeyError:
                 raise TypeError("Interface %s not supported" % cls._iid_)
         return value.QueryInterface(cls.__com_interface__)
-
-
-################################################################
-
-
-class BSTR(_SimpleCData):
-    "The windows BSTR data type"
-    _type_ = "X"
-    _needsfree = False
-
-    def __repr__(self):
-        return "%s(%r)" % (self.__class__.__name__, self.value)
-
-    def __ctypes_from_outparam__(self):
-        self._needsfree = True
-        return self.value
-
-    def __del__(self, _free=windll.oleaut32.SysFreeString):
-        # Free the string if self owns the memory
-        # or if instructed by __ctypes_from_outparam__.
-        if self._b_base_ is None or self._needsfree:
-            _free(self)
-
-    @classmethod
-    def from_param(cls, value):
-        """Convert into a foreign function call parameter."""
-        if isinstance(value, cls):
-            return value
-        # Although the builtin SimpleCData.from_param call does the
-        # right thing, it doesn't ensure that SysFreeString is called
-        # on destruction.
-        return cls(value)
 
 
 ################################################################
