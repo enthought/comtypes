f21b6d0bfede2aaa424b2ddb2e166e71b74e6c71
junkmd <jun.k.job.finding12@gmail.com>
2024-06-08 22:27:58 +0900

    import idl stuffs from `_idl_stuff`

--- comtypes/__init__.py
+++ comtypes/__init__.py
@@ -53,7 +53,8 @@ from comtypes._memberspec import (
 from comtypes._tlib_version_checker import _check_version  # noqa
 from comtypes._bstr import BSTR  # noqa
 from comtypes._py_instance_method import instancemethod
-
+from comtypes._idl_stuff import defaultvalue, helpstring, dispid  # noqa
+from comtypes._idl_stuff import STDMETHOD, DISPMETHOD, DISPPROPERTY, COMMETHOD  # noqa
 
 _all_slice = slice(None, None, None)
 
@@ -638,76 +639,6 @@ class _compointer_base(c_void_p, metaclass=_compointer_meta):
             except KeyError:
                 raise TypeError("Interface %s not supported" % cls._iid_)
         return value.QueryInterface(cls.__com_interface__)
-
-
-################################################################
-# IDL stuff
-
-
-class helpstring(str):
-    "Specifies the helpstring for a COM method or property."
-
-
-class defaultvalue(object):
-    "Specifies the default value for parameters marked optional."
-
-    def __init__(self, value):
-        self.value = value
-
-
-class dispid(int):
-    "Specifies the DISPID of a method or property."
-
-
-# XXX STDMETHOD, COMMETHOD, DISPMETHOD, and DISPPROPERTY should return
-# instances with more methods or properties, and should not behave as an unpackable.
-
-
-def STDMETHOD(restype, name, argtypes=()) -> _ComMemberSpec:
-    "Specifies a COM method slot without idlflags"
-    return _ComMemberSpec(restype, name, argtypes, None, (), None)
-
-
-def DISPMETHOD(idlflags, restype, name, *argspec) -> _DispMemberSpec:
-    "Specifies a method of a dispinterface"
-    return _DispMemberSpec("DISPMETHOD", name, tuple(idlflags), restype, argspec)
-
-
-def DISPPROPERTY(idlflags, proptype, name) -> _DispMemberSpec:
-    "Specifies a property of a dispinterface"
-    return _DispMemberSpec("DISPPROPERTY", name, tuple(idlflags), proptype, ())
-
-
-# tuple(idlflags) is for the method itself: (dispid, 'readonly')
-
-# sample generated code:
-#     DISPPROPERTY([5, 'readonly'], OLE_YSIZE_HIMETRIC, 'Height'),
-#     DISPMETHOD(
-#         [6], None, 'Render', ([], c_int, 'hdc'), ([], c_int, 'x'), ([], c_int, 'y')
-#     )
-
-
-def COMMETHOD(idlflags, restype, methodname, *argspec) -> _ComMemberSpec:
-    """Specifies a COM method slot with idlflags.
-
-    XXX should explain the sematics of the arguments.
-    """
-    # collect all helpstring instances
-    # We should suppress docstrings when Python is started with -OO
-    # join them together(does this make sense?) and replace by None if empty.
-    helptext = "".join(t for t in idlflags if isinstance(t, helpstring)) or None
-    paramflags, argtypes = _resolve_argspec(argspec)
-    if "propget" in idlflags:
-        name = "_get_%s" % methodname
-    elif "propput" in idlflags:
-        name = "_set_%s" % methodname
-    elif "propputref" in idlflags:
-        name = "_setref_%s" % methodname
-    else:
-        name = methodname
-    return _ComMemberSpec(
-        restype, name, argtypes, paramflags, tuple(idlflags), helptext
-    )
 
 
 ################################################################
