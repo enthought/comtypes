d455d16de6fae817d68ce4ebbf013bea92e11eac
junkmd <jun.k.job.finding12@gmail.com>
2024-06-08 22:27:58 +0900

    import `_check_version` from `_check_version`

--- comtypes/__init__.py
+++ comtypes/__init__.py
@@ -50,6 +50,7 @@ from comtypes._memberspec import (
     _encode_idl,
     _resolve_argspec,
 )
+from comtypes._tlib_version_checker import _check_version  # noqa
 
 
 _all_slice = slice(None, None, None)
@@ -69,22 +70,6 @@ logger = logging.getLogger(__name__)
 #    No handlers could be found for logger "comtypes"
 # when logging is not configured and logger.error() is called.
 logger.addHandler(NullHandler())
-
-
-def _check_version(actual, tlib_cached_mtime=None):
-    from comtypes.tools.codegenerator import version as required
-
-    if actual != required:
-        raise ImportError("Wrong version")
-    if not hasattr(sys, "frozen"):
-        g = sys._getframe(1).f_globals
-        tlb_path = g.get("typelib_path")
-        try:
-            tlib_curr_mtime = os.stat(tlb_path).st_mtime
-        except (OSError, TypeError):
-            return
-        if not tlib_cached_mtime or abs(tlib_curr_mtime - tlib_cached_mtime) >= 1:
-            raise ImportError("Typelib different than module")
 
 
 pythonapi.PyInstanceMethod_New.argtypes = [py_object]
