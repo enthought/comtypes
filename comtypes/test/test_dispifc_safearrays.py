a552fbe001a827e74ac59d20137dc436dc0e1b4c
Thomas Geppert <geppi@users.noreply.github.com>
2024-06-24 18:36:02 +0200

    Apply suggestions from code review
    Co-authored-by: Jun Komoda <45822440+junkmd@users.noreply.github.com>

--- comtypes/test/test_dispifc_safearrays.py
+++ comtypes/test/test_dispifc_safearrays.py
@@ -22,8 +22,8 @@ except (ImportError, OSError):
 class Test_DispMethods(unittest.TestCase):
     """Test dispmethods with safearray and safearray pointer parameters."""
 
-    ZERO_ARRAY = tuple([0.0 for i in range(10)])
-    EXPECTED_INITED_ARRAY = tuple([float(i) for i in range(10)])
+    UNPACKED_ZERO_VALS = tuple(0.0 for _ in range(10))
+    UNPACKED_CONSECUTIVE_VALS = tuple(float(i) for i in range(10))
 
     def _create_dispifc(self) -> "ComtypesCppTestSrvLib.IDispSafearrayParamTest":
         # Explicitely ask for the dispinterface of the component.
@@ -33,29 +33,35 @@ class Test_DispMethods(unittest.TestCase):
             interface=ComtypesCppTestSrvLib.IDispSafearrayParamTest,
         )
 
+    def _create_zero_array(self) -> comtypes.safearray._midlSAFEARRAY:
+        return comtypes.safearray._midlSAFEARRAY(c_double).create(
+            [c_double() for _ in range(10)]
+        )
+
+    def _create_consecutive_array(self) -> comtypes.safearray._midlSAFEARRAY:
+        return comtypes.safearray._midlSAFEARRAY(c_double).create(
+            [c_double(i) for i in range(10)]
+        )
+
     def test_inout_byref(self):
         dispifc = self._create_dispifc()
         # Passing a safearray by reference to a method that has declared the parameter
         # as [in, out] we expect modifications of the safearray on the server side to
         # also change the safearray on the client side.
-        test_array = comtypes.safearray._midlSAFEARRAY(c_double).create(
-            [c_double() for i in range(10)]
-        )
-        self.assertEqual(test_array.unpack(), self.ZERO_ARRAY)
+        test_array = self._create_zero_array()
+        self.assertEqual(test_array.unpack(), self.UNPACKED_ZERO_VALS)
         dispifc.InitArray(byref(test_array))
-        self.assertEqual(test_array.unpack(), self.EXPECTED_INITED_ARRAY)
+        self.assertEqual(test_array.unpack(), self.UNPACKED_CONSECUTIVE_VALS)
 
     def test_inout_pointer(self):
         dispifc = self._create_dispifc()
         # Passing a safearray pointer to a method that has declared the parameter
         # as [in, out] we expect modifications of the safearray on the server side to
         # also change the safearray on the client side.
-        test_array = comtypes.safearray._midlSAFEARRAY(c_double).create(
-            [c_double() for i in range(10)]
-        )
-        self.assertEqual(test_array.unpack(), self.ZERO_ARRAY)
-        dispifc.InitArray(pointer(test_array))
-        self.assertEqual(test_array.unpack(), self.EXPECTED_INITED_ARRAY)
+        test_array = self._create_zero_array()
+        self.assertEqual(test_array.unpack(), self.UNPACKED_ZERO_VALS)
+        dispifc.InitArray(byref(test_array))
+        self.assertEqual(test_array.unpack(), self.UNPACKED_CONSECUTIVE_VALS)
 
     def test_in_safearray(self):
         # Passing a safearray to a method that has declared the parameter just as [in]
@@ -65,23 +71,17 @@ class Test_DispMethods(unittest.TestCase):
         # the server side. For this, the 'VerifyArray' method returns 'True' if
         # the safearray items have values equal to the initialization values
         # provided by 'InitArray'.
-        inited_array = comtypes.safearray._midlSAFEARRAY(c_double).create(
-            [c_double(i) for i in range(10)]
-        )
-        # Also perform the inverted test. For this, create a safearray with zeros.
-        zero_array = comtypes.safearray._midlSAFEARRAY(c_double).create(
-            [c_double() for i in range(10)]
-        )
-        for sa, expected, array_content in [
-            (inited_array, True, self.EXPECTED_INITED_ARRAY),
-            (zero_array, False, self.ZERO_ARRAY),
+        for sa, expected, unpacked_content in [
+            (self._create_consecutive_array(), True, self.UNPACKED_CONSECUTIVE_VALS),
+            # Also perform the inverted test. For this, create a safearray with zeros.
+            (self._create_zero_array(), False, self.UNPACKED_ZERO_VALS),
         ]:
-            with self.subTest(expected=expected, array_content=array_content):
+            with self.subTest(expected=expected, unpacked_content=unpacked_content):
                 # Perform the check on initialization values.
                 self.assertEqual(self._create_dispifc().VerifyArray(sa), expected)
                 # Check if the safearray is unchanged although the method
                 # modifies the safearray on the server side.
-                self.assertEqual(sa.unpack(), array_content)
+                self.assertEqual(sa.unpack(), unpacked_content)
 
 
 if __name__ == "__main__":
