3c3448e4ff9ed87e8b9eb39f0a5b49a9a55f818b
Jun Komoda <45822440+junkmd@users.noreply.github.com>
2024-07-19 21:58:37 +0900

    Avoid using keywords in `TYPE_CHECKING` blocks. (#589)
    * Avoid using a keyword as a member name.
    If a keyword is used as a member name, replace it with `pass` and a comment.
    
    * Add `test_typeannotator`.
    
    Code within `TYPE_CHECKING` blocks are not executed at runtime, so they will
    not result in `NameError` or `AttributeError`.
    However, if method names or argument names use Python reserved words, or if
    the statements or expressions are incorrect in Python syntax, a `SyntaxError`
    will be raised.
    
    The newly added tests ensure that `typeannotator` avoids such situations:
    - If there are method names that match Python reserved words,
      `typeannotator` replaces them with `pass` and comments.
    - If there are argument names that match Python reserved words,
      `typeannotator` replaces them with `*args` or `**kwargs`.
    - If there are methods that specify required arguments after optional
      arguments, `typeannotator` replaces them with `**kwargs`.
    
    It was challenging to find a "real-world" COM library that covered all these
    cases. Therefore, in these tests, using `typedesc`s as a test double to
    simulate the results of parsing a COM type library.
    
    It has been confirmed through the discussions in the following issue that
    the changes made to the production code in commit 35f3336 prevent errors from
    occurring in a real COM library.
    https://github.com/enthought/comtypes/issues/588#issuecomment-2230209573

--- comtypes/tools/codegenerator/typeannotator.py
+++ comtypes/tools/codegenerator/typeannotator.py
@@ -114,6 +114,8 @@ class _MethodsAnnotator(abc.ABC, Generic[_T_MTD]):
             content = f"{mem_name} = hints.named_property('{mem_name}', fset={setter})"
         else:
             return
+        if keyword.iskeyword(mem_name):
+            content = f"pass  # avoid using a keyword for {content}"
         self._define_member(content)
 
     def _define_normal_prop(
@@ -127,6 +129,8 @@ class _MethodsAnnotator(abc.ABC, Generic[_T_MTD]):
             content = f"{mem_name} = hints.normal_property(fset={setter})"
         else:
             return
+        if keyword.iskeyword(mem_name):
+            content = f"pass  # avoid using a keyword for {content}"
         self._define_member(content)
 
     def _define_member(self, content: str) -> None:
@@ -253,7 +257,10 @@ class ComMethodAnnotator(_MethodAnnotator[typedesc.ComMethod]):
         else:
             out = "hints.Tuple[" + ", ".join(outargs) + "]"
         in_ = ("self, " + ", ".join(inargs)) if inargs else "self"
-        return f"def {name}({in_}) -> {out}: ..."
+        content = f"def {name}({in_}) -> {out}: ..."
+        if keyword.iskeyword(name):
+            content = f"pass  # avoid using a keyword for {content}"
+        return content
 
 
 class ComMethodsAnnotator(_MethodsAnnotator[typedesc.ComMethod]):
@@ -290,7 +297,10 @@ class DispMethodAnnotator(_MethodAnnotator[typedesc.DispMethod]):
                 has_optional = True
         out = _to_outtype(self.method.returns)
         in_ = ("self, " + ", ".join(inargs)) if inargs else "self"
-        return f"def {name}({in_}) -> {out}: ..."
+        content = f"def {name}({in_}) -> {out}: ..."
+        if keyword.iskeyword(name):
+            content = f"pass  # avoid using a keyword for {content}"
+        return content
 
 
 class DispMethodsAnnotator(_MethodsAnnotator[typedesc.DispMethod]):
@@ -318,9 +328,14 @@ class DispInterfaceMembersAnnotator(object):
         props, methods = self._categorize_members()
         property_lines: List[str] = []
         for mem in props:
-            property_lines.append("@property  # dispprop")
             out = _to_outtype(mem.typ)
-            property_lines.append(f"def {mem.name}(self) -> {out}: ...")
+            decorator = "@property  # dispprop"
+            content = f"def {mem.name}(self) -> {out}: ..."
+            if keyword.iskeyword(mem.name):
+                decorator = f"pass  # {decorator}"
+                content = f"pass  # avoid using a keyword for {content}"
+            property_lines.append(decorator)
+            property_lines.append(content)
         dispprops = "\n".join(f"        {p}" for p in property_lines)
         dispmethods = DispMethodsAnnotator().generate(methods)
         return "\n".join(d for d in (dispprops, dispmethods) if d)
