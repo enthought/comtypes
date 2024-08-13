2c21b19f6ca60fdf334ce299d9a0f2649198bbe3
Jun Komoda <45822440+junkmd@users.noreply.github.com>
2024-08-13 00:20:50 +0000

    add type annotations to `bstr.py`

--- comtypes/_post_coinit/bstr.py
+++ comtypes/_post_coinit/bstr.py
@@ -1,4 +1,8 @@
 from ctypes import _SimpleCData, windll
+from typing import Any, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from comtypes import hints  # type: ignore
 
 
 class BSTR(_SimpleCData):
@@ -6,21 +10,21 @@ class BSTR(_SimpleCData):
     _type_ = "X"
     _needsfree = False
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return "%s(%r)" % (self.__class__.__name__, self.value)
 
-    def __ctypes_from_outparam__(self):
+    def __ctypes_from_outparam__(self) -> Any:
         self._needsfree = True
         return self.value
 
-    def __del__(self, _free=windll.oleaut32.SysFreeString):
+    def __del__(self, _free=windll.oleaut32.SysFreeString) -> None:
         # Free the string if self owns the memory
         # or if instructed by __ctypes_from_outparam__.
         if self._b_base_ is None or self._needsfree:
             _free(self)
 
     @classmethod
-    def from_param(cls, value):
+    def from_param(cls, value: Any) -> "hints.Self":
         """Convert into a foreign function call parameter."""
         if isinstance(value, cls):
             return value
