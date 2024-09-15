2a01f8e8f3db27e6d8ca9a054a6ab1de21e169ca
junkmd <jun.k.job.finding12@gmail.com>
2024-09-15 09:45:08 +0900

    Add `Test_RemoteSeek` to `test_stream`.

--- comtypes/test/test_stream.py
+++ comtypes/test/test_stream.py
@@ -10,6 +10,8 @@ from comtypes.gen.PortableDeviceApiLib import IStream
 
 STGC_DEFAULT = 0
 STREAM_SEEK_SET = 0
+STREAM_SEEK_CUR = 1
+STREAM_SEEK_END = 2
 
 
 def _create_stream() -> IStream:
@@ -50,6 +52,39 @@ class Test_RemoteRead(ut.TestCase):
         # Verification
         self.assertEqual(data_read, len(test_data))
         self.assertEqual(bytearray(read_buffer)[0:data_read], test_data)
+
+
+class Test_RemoteSeek(ut.TestCase):
+    def _create_sample_stream(self) -> IStream:
+        stream = _create_stream()
+        test_data = b"spam egg bacon ham"
+        pv = (c_ubyte * len(test_data)).from_buffer(bytearray(test_data))
+        stream.RemoteWrite(pv, len(test_data))
+        stream.Commit(STGC_DEFAULT)
+        return stream
+
+    def test_takes_STREAM_SEEK_SET_as_origin(self):
+        stream = self._create_sample_stream()
+        newpos = stream.RemoteSeek(9, STREAM_SEEK_SET)
+        self.assertEqual(newpos, 9)
+        buf, read = stream.RemoteRead(1024)
+        self.assertEqual(bytearray(buf)[0:read], b"bacon ham")
+
+    def test_takes_STREAM_SEEK_CUR_as_origin(self):
+        stream = self._create_sample_stream()
+        stream.RemoteSeek(8, STREAM_SEEK_SET)
+        newpos = stream.RemoteSeek(7, STREAM_SEEK_CUR)
+        self.assertEqual(newpos, 15)
+        buf, read = stream.RemoteRead(1024)
+        self.assertEqual(bytearray(buf)[0:read], b"ham")
+
+    def test_takes_STREAM_SEEK_END_as_origin(self):
+        stream = self._create_sample_stream()
+        stream.RemoteSeek(8, STREAM_SEEK_SET)
+        newpos = stream.RemoteSeek(-13, STREAM_SEEK_END)
+        self.assertEqual(newpos, 5)
+        buf, read = stream.RemoteRead(1024)
+        self.assertEqual(bytearray(buf)[0:read], b"egg bacon ham")
 
 
 if __name__ == "__main__":
