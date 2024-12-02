d6b38e57dfb3d80e915e2db80916c3fa7194ab41
Jun Komoda <45822440+junkmd@users.noreply.github.com>
2024-12-02 11:39:35 +0000

    Remove test section.

--- comtypes/server/w_getopt.py
+++ comtypes/server/w_getopt.py
@@ -41,35 +41,3 @@ def w_getopt(args, options):
             args = args[1:]
 
     return opts, arguments
-
-
-if __debug__:
-    if __name__ == "__main__":
-        import unittest
-
-        class TestCase(unittest.TestCase):
-            def test_1(self):
-                args = "-embedding spam /RegServer foo /UnregSERVER blabla".split()
-                opts, args = w_getopt(args, "regserver unregserver embedding".split())
-                self.assertEqual(
-                    opts, [("embedding", ""), ("regserver", ""), ("unregserver", "")]
-                )
-                self.assertEqual(args, ["spam", "foo", "blabla"])
-
-            def test_2(self):
-                args = "/TLB Hello.Tlb HELLO.idl".split()
-                opts, args = w_getopt(args, ["tlb:"])
-                self.assertEqual(opts, [("tlb", "Hello.Tlb")])
-                self.assertEqual(args, ["HELLO.idl"])
-
-            def test_3(self):
-                # Invalid option
-                self.assertRaises(
-                    GetoptError, w_getopt, "/TLIB hello.tlb hello.idl".split(), ["tlb:"]
-                )
-
-            def test_4(self):
-                # Missing argument
-                self.assertRaises(GetoptError, w_getopt, "/TLB".split(), ["tlb:"])
-
-        unittest.main()
