b47957da1dbb369f47eaf5566376c30cf6c96f90
Jun Komoda <45822440+junkmd@users.noreply.github.com>
2024-12-02 11:39:35 +0000

    Update test section.

--- comtypes/test/test_w_getopt.py
+++ comtypes/test/test_w_getopt.py
@@ -1,50 +1,8 @@
-class GetoptError(Exception):
-    pass
+from comtypes.server.w_getopt import GetoptError, w_getopt
 
 
-def w_getopt(args, options):
-    """A getopt for Windows.
-
-    Options may start with either '-' or '/', the option names may
-    have more than one letter (/tlb or -RegServer), and option names
-    are case insensitive.
-
-    Returns two elements, just as getopt.getopt.  The first is a list
-    of (option, value) pairs in the same way getopt.getopt does, but
-    there is no '-' or '/' prefix to the option name, and the option
-    name is always lower case.  The second is the list of arguments
-    which do not belong to an option.
-
-    Different from getopt.getopt, a single argument not belonging to an option
-    does not terminate parsing.
-    """
-    opts = []
-    arguments = []
-    while args:
-        if args[0][:1] in "/-":
-            arg = args[0][1:]  # strip the '-' or '/'
-            arg = arg.lower()
-
-            if arg + ":" in options:
-                try:
-                    opts.append((arg, args[1]))
-                except IndexError:
-                    raise GetoptError(f"option '{args[0]}' requires an argument")
-                args = args[1:]
-            elif arg in options:
-                opts.append((arg, ""))
-            else:
-                raise GetoptError(f"invalid option '{args[0]}'")
-            args = args[1:]
-        else:
-            arguments.append(args[0])
-            args = args[1:]
-
-    return opts, arguments
-
-
-if __debug__:
-    if __name__ == "__main__":
+if True:
+    if True:
         import unittest
 
         class TestCase(unittest.TestCase):
