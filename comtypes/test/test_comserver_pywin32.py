ff8b1ee1a406add3c6977a3401c7b5dcf771babf
junkmd <jun.k.job.finding12@gmail.com>
2024-12-08 16:55:36 +0900

    Remove duplicated non-win32com tests from `test_comserver_pywin32`.

--- comtypes/test/test_comserver_pywin32.py
+++ comtypes/test/test_comserver_pywin32.py
@@ -1,11 +1,7 @@
-import doctest
 import unittest
-from ctypes import pointer
 from typing import Any
 
 import comtypes.test.TestComServer
-from comtypes import BSTR
-from comtypes.client import CreateObject
 from comtypes.server.register import register, unregister
 from comtypes.test.find_memleak import find_memleak
 
@@ -32,30 +28,6 @@ class BaseServerTest(object):
     def _find_memleak(self, func):
         bytes = find_memleak(func)
         self.assertFalse(bytes, "Leaks %d bytes" % bytes)  # type: ignore
-
-    def test_mixedinout(self):
-        o = self.create_object()
-        self.assertEqual(o.MixedInOut(2, 4), (3, 5))  # type: ignore
-
-    def test_getname(self):
-        # This tests a tricky bug, introduced with this patch:
-        # http://www.python.org/sf/1643874
-        #
-        # Returning a BSTR as an [out] parameter from a server
-        # implementation must transfer the ownership to the caller.
-        # When this is not done, the BSTR instance is SysFreeString'd
-        # too early, and the memory is reused.
-        obj = self.create_object()
-        pb = pointer(BSTR())
-        # Get the BSTR from the server:
-        obj._ITestComServer__com__get_name(pb)
-        # Retrieve the value, but keep the pointer to the BSTR alive:
-        name = pb[0]
-        # Create sme BSTR's to reuse the memory in case it has been freed:
-        for i in range(10):
-            BSTR("f" * len(name))
-        # Make sure the pointer is still valid:
-        self.assertEqual(pb[0], name)  # type: ignore
 
     def test_get_id(self):
         obj = self.create_object()
@@ -90,35 +62,11 @@ class BaseServerTest(object):
         self.assertEqual(func(), (1, 2, 3))  # type: ignore
         self._find_memleak(func)
 
-    def test_get_typeinfo(self):
-        obj = self.create_object()
-
-        def func():
-            obj.GetTypeInfo(0)
-            obj.GetTypeInfoCount()
-            obj.QueryInterface(comtypes.IUnknown)
-
-        self._find_memleak(func)
-
-
-class TestInproc(BaseServerTest, unittest.TestCase):
-    def create_object(self):
-        return CreateObject(
-            "TestComServerLib.TestComServer", clsctx=comtypes.CLSCTX_INPROC_SERVER
-        )
-
-
-class TestLocalServer(BaseServerTest, unittest.TestCase):
-    def create_object(self):
-        return CreateObject(
-            "TestComServerLib.TestComServer", clsctx=comtypes.CLSCTX_LOCAL_SERVER
-        )
-
-    @unittest.skip("This fails. Why?")
-    def test_get_typeinfo(self):
-        # Calling `GetTypeInfo` occurs;
-        # OSError: exception: access violation reading 0x0000000000000000
-        pass
+    # These tests make no sense with win32com, some of the tests in
+    # `test_comserver` are not performed here:
+    # `test_get_typeinfo`, `test_getname` and `test_mixedinout`.
+    # Not sure about `test_mixedinout`; it raise 'Invalid Number of parameters'
+    # Is mixed [in], [out] args not compatible with IDispatch???
 
 
 try:
@@ -127,102 +75,15 @@ except ImportError:
     pass
 else:
 
-    @unittest.skip("This depends on 'pywin32'.")
-    class TestInproc_win32com(TestInproc):
+    class TestInproc(BaseServerTest, unittest.TestCase):
         def create_object(self):
             return Dispatch("TestComServerLib.TestComServer")
 
-        # These tests make no sense with win32com, override to disable them:
-        def test_get_typeinfo(self):
-            pass
-
-        def test_getname(self):
-            pass
-
-        def test_mixedinout(self):
-            # Not sure about this; it raise 'Invalid Number of parameters'
-            # Is mixed [in], [out] args not compatible with IDispatch???
-            pass
-
-    @unittest.skip("This depends on 'pywin32'.")
-    class TestLocalServer_win32com(TestInproc_win32com):
+    class TestLocalServer(BaseServerTest, unittest.TestCase):
         def create_object(self):
             return Dispatch(
                 "TestComServerLib.TestComServer", clsctx=comtypes.CLSCTX_LOCAL_SERVER
             )
-
-
-class TestEvents(unittest.TestCase):
-    def test(self):
-        import comtypes.test.test_comserver
-
-        doctest.testmod(
-            comtypes.test.test_comserver, verbose=False, optionflags=doctest.ELLIPSIS
-        )
-
-
-class ShowEventsExamples:
-    # The following functions are never called, they only contain doctests:
-
-    def ShowEventsFloat(self):
-        """
-        >>> from comtypes.client import CreateObject, ShowEvents
-        >>>
-        >>> o = CreateObject("TestComServerLib.TestComServer")
-        >>> con = ShowEvents(o)
-        # event found: ITestComServerEvents_EvalStarted
-        # event found: ITestComServerEvents_EvalCompleted
-        >>> result = o.eval("10. / 4")
-        Event ITestComServerEvents_EvalStarted(None, '10. / 4')
-        Event ITestComServerEvents_EvalCompleted(None, '10. / 4', VARIANT(vt=0x5, 2.5))
-        >>> result
-        2.5
-        >>>
-        """
-
-    # # The following test, if enabled, works but the testsuit
-    # # crashes elsewhere.  Is there s problem with SAFEARRAYs?
-
-    # def Fails(self):
-    #     """
-    #     >>> from comtypes.client import CreateObject, ShowEvents
-    #     >>>
-    #     >>> o = CreateObject("TestComServerLib.TestComServer")
-    #     >>> con = ShowEvents(o)
-    #     # event found: ITestComServerEvents_EvalStarted
-    #     # event found: ITestComServerEvents_EvalCompleted
-    #     >>> result = o.eval("['32'] * 2")
-    #     Event ITestComServerEvents_EvalStarted(None, u"['32'] * 2")
-    #     Event ITestComServerEvents_EvalCompleted(None, u"['32'] * 2", VARIANT(vt=0x200c, (u'32', u'32')))
-    #     >>> result
-    #     (u'32', u'32')
-    #     >>>
-    #     """
-
-    def GetEvents(self):
-        """
-        >>> from comtypes.client import CreateObject, GetEvents
-        >>>
-        >>> o =  CreateObject("TestComServerLib.TestComServer")
-        >>> class EventHandler(object):
-        ...     def EvalStarted(self, this, what):
-        ...         print("EvalStarted: %s" % what)
-        ...         return 0
-        ...     def EvalCompleted(self, this, what, result):
-        ...         print("EvalCompleted: %s = %s" % (what, result.value))
-        ...         return 0
-        ...
-        >>>
-        >>> con = GetEvents(o, EventHandler())
-        >>> o.eval("2 + 3")
-        EvalStarted: 2 + 3
-        EvalCompleted: 2 + 3 = 5
-        5
-        >>> del con
-        >>> o.eval("3 + 2")
-        5
-        >>>
-        """
 
 
 if __name__ == "__main__":
