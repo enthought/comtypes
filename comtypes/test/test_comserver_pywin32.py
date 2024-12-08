e8aa5fff6a9db26504a1fbc3e0155d21a839e160
junkmd <jun.k.job.finding12@gmail.com>
2024-12-08 16:55:36 +0900

    Move win32com stuff from `test_dispinterface` to `test_comserver_pywin32`.

--- comtypes/test/test_comserver_pywin32.py
+++ comtypes/test/test_comserver_pywin32.py
@@ -2,11 +2,13 @@ import unittest
 from typing import Any
 
 import comtypes.test.TestComServer
+import comtypes.test.TestDispServer
 from comtypes.server.register import register, unregister
 from comtypes.test.find_memleak import find_memleak
 
 try:
     from win32com.client import Dispatch
+    from win32com.client.gencache import EnsureDispatch
 
     IMPORT_FAILED = False
 except ImportError:
@@ -18,6 +20,7 @@ def setUpModule():
         raise unittest.SkipTest("This depends on 'pywin32'.")
     try:
         register(comtypes.test.TestComServer.TestComServer)
+        register(comtypes.test.TestDispServer.TestDispServer)
     except WindowsError as e:
         if e.winerror != 5:  # [Error 5] Access is denied
             raise e
@@ -28,6 +31,7 @@ def setUpModule():
 
 
 def tearDownModule():
+    unregister(comtypes.test.TestDispServer.TestDispServer)
     unregister(comtypes.test.TestComServer.TestComServer)
 
 
@@ -88,6 +92,73 @@ class TestLocalServer(BaseServerTest, unittest.TestCase):
         return Dispatch(
             "TestComServerLib.TestComServer", clsctx=comtypes.CLSCTX_LOCAL_SERVER
         )
+
+
+class Test_win32com_Dispatch(unittest.TestCase):
+    def test_win32com_ensure_dispatch(self):
+        # EnsureDispatch is case-sensitive
+        d = EnsureDispatch("TestDispServerLib.TestDispServer")
+
+        self.assertEqual(d.eval("3.14"), 3.14)
+        self.assertEqual(d.eval("1 + 2"), 3)
+        self.assertEqual(d.eval("[1 + 2, 'foo', None]"), (3, "foo", None))
+
+        self.assertEqual(d.eval2("3.14"), 3.14)
+        self.assertEqual(d.eval2("1 + 2"), 3)
+        self.assertEqual(d.eval2("[1 + 2, 'foo', None]"), (3, "foo", None))
+
+        d.eval(
+            "__import__('comtypes.client').client.CreateObject('Scripting.Dictionary')"
+        )
+
+        server_id = d.eval("id(self)")
+        self.assertEqual(d.id, server_id)
+
+        self.assertEqual(d.name, "spam, spam, spam")
+
+        d.SetName("foo bar")
+        self.assertEqual(d.name, "foo bar")
+
+        d.name = "blah"
+        self.assertEqual(d.name, "blah")
+
+    def test_win32com_dynamic_dispatch(self):
+        # dynamic Dispatch is case-IN-sensitive
+        d = Dispatch("TestDispServerLib.TestDispServer")
+
+        self.assertEqual(d.eval("3.14"), 3.14)
+        self.assertEqual(d.eval("1 + 2"), 3)
+        self.assertEqual(d.eval("[1 + 2, 'foo', None]"), (3, "foo", None))
+
+        self.assertEqual(d.eval2("3.14"), 3.14)
+        self.assertEqual(d.eval2("1 + 2"), 3)
+        self.assertEqual(d.eval2("[1 + 2, 'foo', None]"), (3, "foo", None))
+
+        d.eval(
+            "__import__('comtypes.client').client.CreateObject('Scripting.Dictionary')"
+        )
+
+        self.assertEqual(d.EVAL("3.14"), 3.14)
+        self.assertEqual(d.EVAL("1 + 2"), 3)
+        self.assertEqual(d.EVAL("[1 + 2, 'foo', None]"), (3, "foo", None))
+
+        self.assertEqual(d.EVAL2("3.14"), 3.14)
+        self.assertEqual(d.EVAL2("1 + 2"), 3)
+        self.assertEqual(d.EVAL2("[1 + 2, 'foo', None]"), (3, "foo", None))
+
+        server_id = d.eval("id(self)")
+        self.assertEqual(d.id, server_id)
+        self.assertEqual(d.ID, server_id)
+
+        self.assertEqual(d.Name, "spam, spam, spam")
+        self.assertEqual(d.nAME, "spam, spam, spam")
+
+        d.SetName("foo bar")
+        self.assertEqual(d.Name, "foo bar")
+
+        # fails.  Why?
+        # d.name = "blah"
+        # self.assertEqual(d.Name, "blah")
 
 
 if __name__ == "__main__":
