68ff423c9e3737991127d21cb2fb08e824b8ce69
junkmd <jun.k.job.finding12@gmail.com>
2024-12-09 23:45:20 +0900

    Add `PropPutRefTest` to `test_comserver`.

--- comtypes/test/test_comserver.py
+++ comtypes/test/test_comserver.py
@@ -208,6 +208,35 @@ class SafeArrayTest(unittest.TestCase):
         self.assertFalse(bytes, "Leaks %d bytes" % bytes)
 
 
+class PropPutRefTest(unittest.TestCase):
+    def test(self, dynamic=False):
+        d = CreateObject("Scripting.Dictionary", dynamic=dynamic)
+        s = CreateObject("TestComServerLib.TestComServer", dynamic=dynamic)
+        s.name = "the value"
+
+        # This calls propputref, since we assign an Object
+        d.Item["object"] = s
+        # This calls propput, since we assing a Value
+        d.Item["value"] = s.name
+
+        self.assertEqual(d.Item["object"], s)
+        self.assertEqual(d.Item["object"].name, "the value")
+        self.assertEqual(d.Item["value"], "the value")
+
+        # Changing the default property of the object
+        s.name = "foo bar"
+        self.assertEqual(d.Item["object"], s)
+        self.assertEqual(d.Item["object"].name, "foo bar")
+        self.assertEqual(d.Item["value"], "the value")
+
+        # This also calls propputref since we assign an Object
+        d.Item["var"] = VARIANT(s)
+        self.assertEqual(d.Item["var"], s)
+
+    def test_dispatch(self):
+        return self.test(dynamic=True)
+
+
 class TestEvents(unittest.TestCase):
     def test(self):
         import comtypes.test.test_comserver
