00c1f8a1c92b3045dcb5e514530dd34082408788
junkmd <jun.k.job.finding12@gmail.com>
2024-12-20 00:38:16 +0900

    Add `Test_IUnknown_QueryInterface`.

--- comtypes/test/test_comobject.py
+++ comtypes/test/test_comobject.py
@@ -1,8 +1,11 @@
+import ctypes
 import unittest as ut
+from ctypes import POINTER, byref, pointer
 
 import comtypes
 import comtypes.client
-from comtypes import IUnknown
+from comtypes import IUnknown, hresult
+from comtypes.automation import IDispatch
 
 comtypes.client.GetModule("UIAutomationCore.dll")
 comtypes.client.GetModule("scrrun.dll")
@@ -32,6 +35,42 @@ class Test_QueryInterface(ut.TestCase):
             scrrun.IDictionary._iid_,
         )
         self.assertEqual(dic.GetIDsOfNames("Add"), [1])
+
+
+class Test_IUnknown_QueryInterface(ut.TestCase):
+    def test_e_pointer(self):
+        hr = scrrun.Dictionary().IUnknown_QueryInterface(
+            None, pointer(scrrun.IDictionary._iid_), ctypes.c_void_p()
+        )
+        self.assertEqual(hr, hresult.E_POINTER)
+
+    def test_e_no_interface(self):
+        hr = scrrun.Dictionary().IUnknown_QueryInterface(
+            None, pointer(uiac.IUIAutomation._iid_), ctypes.c_void_p()
+        )
+        self.assertEqual(hr, hresult.E_NOINTERFACE)
+
+    def test_valid_pointer(self):
+        ptr = ctypes.c_void_p()
+        ctypes.oledll.ole32.CoCreateInstance(
+            byref(scrrun.Dictionary._reg_clsid_),
+            None,
+            comtypes.CLSCTX_SERVER,
+            byref(scrrun.IDictionary._iid_),
+            byref(ptr),
+        )
+        hr = scrrun.Dictionary().IUnknown_QueryInterface(
+            None, pointer(scrrun.IDictionary._iid_), ptr
+        )
+        self.assertEqual(hr, hresult.S_OK)
+
+    def test_valid_interface(self):
+        dic = POINTER(IDispatch)()
+        hr = scrrun.Dictionary().IUnknown_QueryInterface(
+            None, pointer(scrrun.IDictionary._iid_), byref(dic)
+        )
+        self.assertEqual(hr, hresult.S_OK)
+        self.assertEqual(dic.GetTypeInfoCount(), 1)  # type: ignore
 
 
 class Test_IPersist_GetClassID(ut.TestCase):
