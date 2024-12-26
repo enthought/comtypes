434e48ea9bf27b5e72e4d8cb7877c0a21d319e9c
Jun Komoda <45822440+junkmd@users.noreply.github.com>
2024-12-26 12:08:33 +0000

    Revert `master_doc` in `conf.py` to reflect the temporary module name change.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,7 +45,7 @@ source_suffix = ".rst"
 # source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = "main"
+master_doc = "index"
 
 # General information about the project.
 project = "comtypes"
