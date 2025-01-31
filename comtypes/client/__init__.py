cdf80c21a9d18ab3e48157b2c397663bea4a61e5
junkmd <jun.k.job.finding12@gmail.com>
2025-01-31 20:02:22 +0900

    Adjust import part of `client/__init__.py`.

--- comtypes/client/__init__.py
+++ comtypes/client/__init__.py
@@ -1,40 +1,23 @@
 """comtypes.client - High level client level COM support package."""
 
-################################################################
-#
-# TODO:
-#
-# - refactor some code into modules
-#
-################################################################
-
 import ctypes
 import logging
-import os
-import sys
-from typing import TYPE_CHECKING, Any, Optional, Type, TypeVar, overload
-from typing import Union as _UnionT
 
-import comtypes
-import comtypes.client.dynamic
-from comtypes import GUID, CoClass, IUnknown, automation, typeinfo
+from comtypes import automation
+from comtypes.client import dynamic  # noqa
 from comtypes.client._code_cache import _find_gen_dir
-from comtypes.client._constants import Constants
+from comtypes.client._constants import Constants  # noqa
 from comtypes.client._events import GetEvents, PumpEvents, ShowEvents
 from comtypes.client._generate import GetModule
 from comtypes.client._misc import GetBestInterface, _manage, wrap_outparam  # noqa
 from comtypes.hresult import *  # noqa
 
-if TYPE_CHECKING:
-    from comtypes import hints  # type: ignore
-
 gen_dir = _find_gen_dir()
-import comtypes.gen
+import comtypes.gen  # noqa
 
 ### for testing
 ##gen_dir = None
 
-_T_IUnknown = TypeVar("_T_IUnknown", bound=IUnknown)
 logger = logging.getLogger(__name__)
 
 
