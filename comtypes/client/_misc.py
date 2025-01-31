eb5f77d73c05b19cd359acb168559712a7e59d79
junkmd <jun.k.job.finding12@gmail.com>
2025-01-31 20:02:21 +0900

    Remove unnecessary stuff from `client/_misc.py`.

--- comtypes/client/_misc.py
+++ comtypes/client/_misc.py
@@ -1,37 +1,16 @@
-"""comtypes.client - High level client level COM support package."""
-
-################################################################
-#
-# TODO:
-#
-# - refactor some code into modules
-#
-################################################################
-
-import ctypes
 import logging
-import os
-import sys
 from typing import TYPE_CHECKING, Any, Optional, Type, TypeVar, overload
 from typing import Union as _UnionT
 
 import comtypes
 import comtypes.client.dynamic
 from comtypes import GUID, CoClass, IUnknown, automation, typeinfo
-from comtypes.client._code_cache import _find_gen_dir
-from comtypes.client._constants import Constants
-from comtypes.client._events import GetEvents, PumpEvents, ShowEvents
 from comtypes.client._generate import GetModule
 from comtypes.hresult import *
 
 if TYPE_CHECKING:
     from comtypes import hints  # type: ignore
 
-gen_dir = _find_gen_dir()
-import comtypes.gen
-
-### for testing
-##gen_dir = None
 
 _T_IUnknown = TypeVar("_T_IUnknown", bound=IUnknown)
 logger = logging.getLogger(__name__)
@@ -130,13 +109,6 @@ def GetBestInterface(punk: Any) -> Any:
     result = punk.QueryInterface(interface)
     logger.debug("Final result is %s", result)
     return result
-
-
-# backwards compatibility:
-wrap = GetBestInterface
-
-# Should we do this for POINTER(IUnknown) also?
-ctypes.POINTER(automation.IDispatch).__ctypes_from_outparam__ = wrap_outparam  # type: ignore
 
 
 ################################################################
@@ -313,11 +285,3 @@ def CoGetObject(
     if dynamic:
         return comtypes.client.dynamic.Dispatch(punk)
     return _manage(punk, clsid=None, interface=interface)
-
-
-# fmt: off
-__all__ = [
-    "CreateObject", "GetActiveObject", "CoGetObject", "GetEvents",
-    "ShowEvents", "PumpEvents", "GetModule", "GetClassObject",
-]
-# fmt: on
