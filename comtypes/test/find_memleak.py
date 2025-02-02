cde054bb796df7ace79741182896816bf89d15cb
junkmd <jun.k.job.finding12@gmail.com>
2025-02-02 23:02:26 +0900

    Remove workarounds for Windows CE in `test/find_memleak.py`.

--- comtypes/test/find_memleak.py
+++ comtypes/test/find_memleak.py
@@ -33,50 +33,40 @@ _GetProcessMemoryInfo = _psapi.GetProcessMemoryInfo
 _GetProcessMemoryInfo.argtypes = [HANDLE, POINTER(PROCESS_MEMORY_COUNTERS), DWORD]
 _GetProcessMemoryInfo.restype = BOOL
 
-try:
-    _GetProcessMemoryInfo.argtypes = (
-        HANDLE,
-        POINTER(PROCESS_MEMORY_COUNTERS),
-        DWORD,
-    )
-except WindowsError:
-    # cannot search for memory leaks on Windows CE
-    def find_memleak(func, loops=None):
-        return 0
 
-else:
+def wss():
+    # Return the working set size (memory used by process)
+    pmi = PROCESS_MEMORY_COUNTERS()
+    if not _GetProcessMemoryInfo(-1, byref(pmi), sizeof(pmi)):
+        raise WinError()
+    return pmi.WorkingSetSize
 
-    def wss():
-        # Return the working set size (memory used by process)
-        pmi = PROCESS_MEMORY_COUNTERS()
-        if not _GetProcessMemoryInfo(-1, byref(pmi), sizeof(pmi)):
-            raise WinError()
-        return pmi.WorkingSetSize
 
-    LOOPS = 10, 1000
+LOOPS = 10, 1000
 
-    def find_memleak(func, loops=LOOPS):
-        # call 'func' several times, so that memory consumption
-        # stabilizes:
-        for j in range(loops[0]):
-            for k in range(loops[1]):
-                func()
-        gc.collect()
-        gc.collect()
-        gc.collect()
-        bytes = wss()
-        # call 'func' several times, recording the difference in
-        # memory consumption before and after the call.  Repeat this a
-        # few times, and return a list containing the memory
-        # consumption differences.
-        for j in range(loops[0]):
-            for k in range(loops[1]):
-                func()
-        gc.collect()
-        gc.collect()
-        gc.collect()
-        # return the increased in process size
-        result = wss() - bytes
-        # Sometimes the process size did decrease, we do not report leaks
-        # in this case:
-        return max(result, 0)
+
+def find_memleak(func, loops=LOOPS):
+    # call 'func' several times, so that memory consumption
+    # stabilizes:
+    for j in range(loops[0]):
+        for k in range(loops[1]):
+            func()
+    gc.collect()
+    gc.collect()
+    gc.collect()
+    bytes = wss()
+    # call 'func' several times, recording the difference in
+    # memory consumption before and after the call.  Repeat this a
+    # few times, and return a list containing the memory
+    # consumption differences.
+    for j in range(loops[0]):
+        for k in range(loops[1]):
+            func()
+    gc.collect()
+    gc.collect()
+    gc.collect()
+    # return the increased in process size
+    result = wss() - bytes
+    # Sometimes the process size did decrease, we do not report leaks
+    # in this case:
+    return max(result, 0)
