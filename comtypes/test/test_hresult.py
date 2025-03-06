719c61213ff4ff707562f9a19c9dbea296886f10
junkmd <jun.k.job.finding12@gmail.com>
2025-03-07 07:55:50 +0900

    Add more parameters.

--- comtypes/test/test_hresult.py
+++ comtypes/test/test_hresult.py
@@ -16,6 +16,13 @@ class Test_HRESULT_FROM_WIN32(ut.TestCase):
             (0, -2147024896),
             (1, -2147024895),
             (-1, -1),
+            (0xFFFF - 3, -2146959364),
+            (0xFFFF - 2, -2146959363),
+            (0xFFFF - 1, -2146959362),
+            (0xFFFF + 0, -2146959361),
+            (0xFFFF + 1, -2147024896),
+            (0xFFFF + 2, -2147024895),
+            (0xFFFF + 3, -2147024894),
         ]:
             with self.subTest(w32=w32, hr=hr):
                 self.assertEqual(comtypes.hresult.HRESULT_FROM_WIN32(w32), hr)
