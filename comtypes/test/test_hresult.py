cc1d017f86f1da3f0ec97d34022f637eb3bbc962
Jun Komoda <45822440+junkmd@users.noreply.github.com>
2025-03-07 14:19:34 +0900

    Add `Test_HRESULT_FROM_WIN32` to `test_hresult`. (#828)

---
+++ comtypes/test/test_hresult.py
@@ -0,0 +1,28 @@
+import unittest as ut
+
+import comtypes.hresult
+
+ERROR_OUTOFMEMORY = 14  # 0xE
+ERROR_INVALID_PARAMETER = 87  # 0x57
+RPC_S_SERVER_UNAVAILABLE = 1722  # 0x6BA
+
+
+class Test_HRESULT_FROM_WIN32(ut.TestCase):
+    def test(self):
+        for w32, hr in [
+            (ERROR_OUTOFMEMORY, comtypes.hresult.E_OUTOFMEMORY),
+            (ERROR_INVALID_PARAMETER, comtypes.hresult.E_INVALIDARG),
+            (RPC_S_SERVER_UNAVAILABLE, comtypes.hresult.RPC_S_SERVER_UNAVAILABLE),
+            (-1, -1),
+            (0, -2147024896),
+            (1, -2147024895),
+            (0xFFFF - 3, -2146959364),
+            (0xFFFF - 2, -2146959363),
+            (0xFFFF - 1, -2146959362),
+            (0xFFFF + 0, -2146959361),
+            (0xFFFF + 1, -2147024896),
+            (0xFFFF + 2, -2147024895),
+            (0xFFFF + 3, -2147024894),
+        ]:
+            with self.subTest(w32=w32, hr=hr):
+                self.assertEqual(comtypes.hresult.HRESULT_FROM_WIN32(w32), hr)
