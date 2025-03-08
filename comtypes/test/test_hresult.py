31eaf0a6bcac309c6b1044a61f6dacbd8b1f264a
junkmd <jun.k.job.finding12@gmail.com>
2025-03-08 09:59:57 +0900

    Add `Test_MAKE_HRESULT` to `test_hresult`.

--- comtypes/test/test_hresult.py
+++ comtypes/test/test_hresult.py
@@ -2,6 +2,26 @@ import unittest as ut
 
 import comtypes.hresult
 
+
+class Test_MAKE_HRESULT(ut.TestCase):
+    def test(self):
+        for sev, fac, code, hr in [
+            (0x0000, 0x0000, 0x0000, 0),
+            (0x0001, 0x0000, 0x0000, -2147483648),
+            (0x0000, 0x0001, 0x0000, 65536),
+            (0x0000, 0x0000, 0x0001, 1),
+            (0x0001, 0xFFFF, 0xFFFF, -1),
+            (0x0000, 0xFFFF, 0xFFFF, -1),
+            (0x0001, 0x0000, 0x0001, -2147483647),
+            (0x0001, 0x0001, 0x0001, -2147418111),
+            (0x0000, 0x0001, 0xFFFF, 131071),
+            (0x0001, 0xFFFF, 0x0000, -65536),
+            (0x0001, 0x0000, 0xFFFF, -2147418113),
+        ]:
+            with self.subTest(sev=sev, fac=fac, code=code, hr=hr):
+                self.assertEqual(comtypes.hresult.MAKE_HRESULT(sev, fac, code), hr)
+
+
 ERROR_OUTOFMEMORY = 14  # 0xE
 ERROR_INVALID_PARAMETER = 87  # 0x57
 RPC_S_SERVER_UNAVAILABLE = 1722  # 0x6BA
