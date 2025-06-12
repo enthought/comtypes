ac2d2a76b9b0233d09dc2914e4e1ac9d7c5cdb3e
moi15moi <moi15moismokerlolilol@gmail.com>
2025-06-12 12:22:36 -0400

    Replace setup.py with pyproject.toml + update test_pip_install.py

--- test_pip_install.py
+++ test_pip_install.py
@@ -21,18 +21,18 @@ class TestPipInstall(unittest.TestCase):
 
     def setUp(self):
         """prepare the same package that is usually uploaded to PyPI"""
-        subprocess.check_call([sys.executable, 'setup.py', 'sdist', '--format=zip'])
+        subprocess.check_call([sys.executable, '-m', 'build', '--sdist'])
 
-        filename_for_upload = 'comtypes-%s.zip' % read_version()
+        filename_for_upload = 'comtypes-%s.tar.gz' % read_version()
         self.target_package = os.path.join(os.getcwd(), 'dist', filename_for_upload)
         self.pip_exe = os.path.join(os.path.dirname(sys.executable), 'Scripts', 'pip.exe')
 
     def test_pip_install(self):
-        """Test that "pip install comtypes-x.y.z.zip" works"""
+        """Test that "pip install comtypes-x.y.z.tar.gz" works"""
         subprocess.check_call([self.pip_exe, 'install', self.target_package])
 
     def test_no_cache_dir_custom_location(self):
-        """Test that 'pip install comtypes-x.y.z.zip --no-cache-dir --target="...\custom location"' works"""
+        """Test that 'pip install comtypes-x.y.z.tar.gz --no-cache-dir --target="...\custom location"' works"""
         custom_dir = os.path.join(os.getcwd(), 'custom location')
         if os.path.exists(custom_dir):
             shutil.rmtree(custom_dir)
