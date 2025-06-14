b59fa6427f327e30e9600bea3ff06e12a2ba03ac
junkmd <jun.k.job.finding12@gmail.com>
2025-06-14 18:00:17 +0900

    Eliminate unnecessary version branch in `VariantTestCase.test_pythonobjects`.

--- comtypes/test/test_variant.py
+++ comtypes/test/test_variant.py
@@ -115,10 +115,7 @@ class VariantTestCase(unittest.TestCase):
         self.assertEqual(result, values)
 
     def test_pythonobjects(self):
-        if sys.version_info >= (3, 0):
-            objects = [None, 42, 3.14, True, False, "abc", "abc", 7]
-        else:
-            objects = [None, 42, 3.14, True, False, "abc", "abc", 7]
+        objects = [None, 42, 3.14, True, False, "abc", "abc", 7]
         for x in objects:
             v = VARIANT(x)
             self.assertEqual(x, v.value)
