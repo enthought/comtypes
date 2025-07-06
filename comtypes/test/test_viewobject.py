91e211b47889217bd691826b76b5d799ec29d417
junkmd <jun.k.job.finding12@gmail.com>
2025-07-06 22:02:45 +0900

    Add `Test_IViewObjectEx` to `test_viewobject`.

--- comtypes/test/test_viewobject.py
+++ comtypes/test/test_viewobject.py
@@ -1,13 +1,23 @@
 import unittest
-from ctypes.wintypes import SIZEL
+from ctypes.wintypes import POINT, RECT, SIZEL
 
 import comtypes.client
 from comtypes import IUnknown
-from comtypes.viewobject import DVASPECT_CONTENT, IAdviseSink, IViewObject, IViewObject2
+from comtypes.viewobject import (
+    DVASPECT_CONTENT,
+    IAdviseSink,
+    IViewObject,
+    IViewObject2,
+    IViewObjectEx,
+)
 
 
 def create_shell_explorer() -> IUnknown:
     return comtypes.client.CreateObject("Shell.Explorer")
+
+
+def create_html_document() -> IUnknown:
+    return comtypes.client.CreateObject("htmlfile")
 
 
 class Test_IViewObject(unittest.TestCase):
@@ -35,3 +45,32 @@ class Test_IViewObject2(unittest.TestCase):
         size = vo2.GetExtent(DVASPECT_CONTENT, -1, None)
         self.assertTrue(size)
         self.assertIsInstance(size, SIZEL)
+
+
+class Test_IViewObjectEx(unittest.TestCase):
+    def test_GetRect(self):
+        vo = create_html_document().QueryInterface(IViewObjectEx)
+        rect = vo.GetRect(DVASPECT_CONTENT)
+        self.assertTrue(rect)
+        self.assertIsInstance(rect, RECT)
+
+    def test_GetViewStatus(self):
+        vo = create_html_document().QueryInterface(IViewObjectEx)
+        status = vo.GetViewStatus()
+        self.assertIsInstance(status, int)
+
+    def test_QueryHitPoint(self):
+        vo = create_html_document().QueryInterface(IViewObjectEx)
+        # It is assumed that the view is not transparent at the origin.
+        bounds = RECT(left=0, top=0, right=100, bottom=100)
+        loc = POINT(x=0, y=0)
+        hit = vo.QueryHitPoint(DVASPECT_CONTENT, bounds, loc, 0)
+        self.assertIsInstance(hit, int)
+
+    def test_QueryHitRect(self):
+        vo = create_html_document().QueryInterface(IViewObjectEx)
+        # It is assumed that the view is not transparent at the origin.
+        bounds = RECT(left=0, top=0, right=100, bottom=100)
+        loc = RECT(left=0, top=0, right=1, bottom=1)
+        hit = vo.QueryHitRect(DVASPECT_CONTENT, bounds, loc, 0)
+        self.assertIsInstance(hit, int)
