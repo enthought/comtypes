3e5345690aa0b26bbb6e32a8a70e18b420892f97
junkmd <jun.k.job.finding12@gmail.com>
2025-07-19 12:50:06 +0900

    Add a simple test for `byref_at`.

--- comtypes/test/test_util.py
+++ comtypes/test/test_util.py
@@ -1,6 +1,21 @@
 import unittest
+from ctypes import POINTER, addressof, c_char, c_double, c_int, cast
+
+import comtypes.util
 
 
-class Test(unittest.TestCase):
-    def test(self):
-        import comtypes.util  # noqa
+class ByrefAtTest(unittest.TestCase):
+    def test_ctypes(self):
+        for ctype, value in [
+            (c_int, 42),
+            (c_double, 3.14),
+            (c_char, b"A"),
+        ]:
+            with self.subTest(ctype=ctype, value=value):
+                obj = ctype(value)
+                # Test with zero offset - should point to the same location
+                ref = comtypes.util.byref_at(obj, 0)
+                ptr = cast(ref, POINTER(ctype))
+                # byref objects don't have contents, but we can cast them to pointers
+                self.assertEqual(addressof(ptr.contents), addressof(obj))
+                self.assertEqual(ptr.contents.value, value)
