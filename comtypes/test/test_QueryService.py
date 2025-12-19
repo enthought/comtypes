e69a39639a74aa509307da3f8a702b38cf8d056e
junkmd <jun.k.job.finding12@gmail.com>
2025-12-20 08:20:13 +0900

    test: Enhance `ISelectionServices` and `IMarkupServices` testing.

--- comtypes/test/test_QueryService.py
+++ comtypes/test/test_QueryService.py
@@ -29,7 +29,28 @@ class TestCase(unittest.TestCase):
         self.assertIsInstance(es, POINTER(mshtml.IHTMLEditServices))
         mc = doc.QueryInterface(mshtml.IMarkupContainer)
         ss = es.GetSelectionServices(mc)
-        self.assertIsInstance(ss, POINTER(mshtml.ISelectionServices))
+        # QueryInterface for `IHTMLDocument3` to access `getElementById`.
+        element = doc.QueryInterface(mshtml.IHTMLDocument3).getElementById("test")
+        # MarkupPointer related tests:
+        ms = doc.QueryInterface(mshtml.IMarkupServices)
+        p_start = ms.CreateMarkupPointer()
+        p_end = ms.CreateMarkupPointer()
+        # QueryInterface for `IHTMLBodyElement` to access `createTextRange`.
+        rng = doc.body.QueryInterface(mshtml.IHTMLBodyElement).createTextRange()
+        rng.moveToElementText(element)
+        ms.MovePointersToRange(rng, p_start, p_end)
+        self.assertTrue(p_start.IsLeftOf(p_end))
+        self.assertTrue(p_end.IsRightOf(p_start))
+        self.assertFalse(p_start.IsEqualTo(p_end))
+        seg = ss.AddSegment(p_start, p_end)
+        q_start = ms.CreateMarkupPointer()
+        q_end = ms.CreateMarkupPointer()
+        self.assertFalse(p_start.IsEqualTo(q_start))
+        self.assertFalse(p_end.IsEqualTo(q_end))
+        seg.GetPointers(q_start, q_end)
+        self.assertTrue(p_start.IsEqualTo(q_start))
+        self.assertTrue(p_end.IsEqualTo(q_end))
+        ss.RemoveSegment(seg)
 
 
 if __name__ == "__main__":
