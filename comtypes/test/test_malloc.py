ed4a8178155a61adab3a590d02de5690b951895e
junkmd <jun.k.job.finding12@gmail.com>
2026-01-10 20:33:32 +0900

    test: Add tests for `IMalloc.Realloc` in `test_malloc.py`.
    Introduces `comtypes/test/test_malloc.py` to provide test coverage
    for the `IMalloc.Realloc` method, ensuring proper memory
    reallocation behavior.

--- comtypes/test/test_malloc.py
+++ comtypes/test/test_malloc.py
@@ -1,6 +1,34 @@
 import unittest as ut
+from ctypes import POINTER, byref
 
-from comtypes.malloc import IMalloc  # noqa
+from comtypes.malloc import IMalloc, _CoGetMalloc
 
 
-class Test(ut.TestCase): ...
+def _get_malloc() -> IMalloc:
+    malloc = POINTER(IMalloc)()
+    _CoGetMalloc(1, byref(malloc))
+    assert bool(malloc)
+    return malloc  # type: ignore
+
+
+class Test(ut.TestCase):
+    def test_Realloc(self):
+        malloc = _get_malloc()
+        size1 = 4
+        ptr1 = malloc.Alloc(size1)
+        self.assertEqual(malloc.DidAlloc(ptr1), 1)
+        self.assertEqual(malloc.GetSize(ptr1), size1)
+        size2 = size1 - 1
+        ptr2 = malloc.Realloc(ptr1, size2)
+        self.assertEqual(malloc.DidAlloc(ptr1), 0)
+        self.assertEqual(malloc.DidAlloc(ptr2), 1)
+        self.assertEqual(malloc.GetSize(ptr2), size2)
+        size3 = size1 + 1
+        ptr3 = malloc.Realloc(ptr2, size3)
+        self.assertEqual(malloc.DidAlloc(ptr2), 0)
+        self.assertEqual(malloc.DidAlloc(ptr3), 1)
+        self.assertEqual(malloc.GetSize(ptr3), size3)
+        malloc.Free(ptr3)
+        self.assertEqual(malloc.DidAlloc(ptr3), 0)
+        malloc.HeapMinimize()
+        del ptr3
