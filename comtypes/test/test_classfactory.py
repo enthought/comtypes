98cf8431f6bf0d7e916b1b9e6ff1612647bb6dbf
junkmd <jun.k.job.finding12@gmail.com>
2026-01-17 23:31:13 +0900

    test: Enhance `IClassFactory.CreateInstance` tests for dynamic dispatch.
    Add tests for `IClassFactory.CreateInstance` with `dynamic=True` to verify
    that it returns `lazybind.Dispatch` when type information is available and
    `dynamic._Dispatch` when it's not.

--- comtypes/test/test_classfactory.py
+++ comtypes/test/test_classfactory.py
@@ -1,11 +1,13 @@
 import unittest as ut
 
+import comtypes.client
 from comtypes import GUID, CoGetClassObject, IUnknown, shelllink
 from comtypes.server import IClassFactory
 
 CLSID_ShellLink = GUID("{00021401-0000-0000-C000-000000000046}")
 
 REGDB_E_CLASSNOTREG = -2147221164  # 0x80040154
+HKCU = 1  # HKEY_CURRENT_USER
 
 
 class Test_CreateInstance(ut.TestCase):
@@ -26,6 +28,25 @@ class Test_CreateInstance(ut.TestCase):
         shlnk = punk.QueryInterface(shelllink.IShellLinkW)
         shlnk.SetDescription("sample")
         self.assertEqual(shlnk.GetDescription(), "sample")
+
+    def test_returns_lazybind_dynamic_dispatch_if_typeinfo_is_available(self):
+        # When `dynamic=True`, objects providing type information will return a
+        # `lazybind.Dispatch` instance.
+        class_factory = comtypes.client.GetClassObject("Scripting.Dictionary")
+        self.assertIsInstance(class_factory, IClassFactory)
+        dic = class_factory.CreateInstance(dynamic=True)
+        self.assertIsInstance(dic, comtypes.client.lazybind.Dispatch)
+        dic.Item["key"] = "value"
+        self.assertEqual(dic.Item["key"], "value")
+
+    def test_returns_fully_dynamic_dispatch_if_typeinfo_is_unavailable(self):
+        # When `dynamic=True`, objects that do NOT provide type information (or
+        # fail to provide it) will return a `dynamic._Dispatch` instance.
+        class_factory = comtypes.client.GetClassObject("WindowsInstaller.Installer")
+        self.assertIsInstance(class_factory, IClassFactory)
+        inst = class_factory.CreateInstance(dynamic=True)
+        self.assertIsInstance(inst, comtypes.client.dynamic._Dispatch)
+        self.assertTrue(inst.RegistryValue(HKCU, r"Control Panel\Desktop"))
 
     def test_raises_valueerror_if_takes_dynamic_true_and_interface_explicitly(self):
         class_factory = CoGetClassObject(CLSID_ShellLink)
