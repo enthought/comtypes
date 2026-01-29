a3daa70a1aac3e37cabb70c0b88f020e5f62d9f4
junkmd <jun.k.job.finding12@gmail.com>
2026-01-30 08:08:08 +0900

    refactor: Use `contextlib.contextmanager` for GIT registration in `test_git`.
    This change introduces a context manager `register_in_global` to streamline
    the registration and revocation of COM objects in the Global Interface Table
    (GIT) within `test_git.py`. This ensures proper cleanup, improving readability.

--- comtypes/test/test_git.py
+++ comtypes/test/test_git.py
@@ -1,5 +1,8 @@
+import contextlib
 import unittest as ut
+from collections.abc import Iterator
 
+from comtypes import IUnknown
 from comtypes.git import (
     GetInterfaceFromGlobal,
     RegisterInterfaceInGlobal,
@@ -8,20 +11,28 @@ from comtypes.git import (
 from comtypes.typeinfo import CreateTypeLib, ICreateTypeLib
 
 
+@contextlib.contextmanager
+def register_in_global(obj: IUnknown) -> Iterator[int]:
+    cookie = RegisterInterfaceInGlobal(obj)
+    try:
+        yield cookie
+    finally:
+        RevokeInterfaceFromGlobal(cookie)
+
+
 class Test(ut.TestCase):
     def test(self):
         tlib = CreateTypeLib("foo.bar")  # we don not save it later
         self.assertEqual((tlib.AddRef(), tlib.Release()), (2, 1))
-        cookie = RegisterInterfaceInGlobal(tlib)
-        # When an object is registered to GIT, `AddRef` is called, incrementing
-        # its reference count. This ensures the object remains valid as long as
-        # it's globally registered in the GIT.
-        self.assertEqual((tlib.AddRef(), tlib.Release()), (3, 2))
-        GetInterfaceFromGlobal(cookie, interface=ICreateTypeLib)
-        GetInterfaceFromGlobal(cookie, interface=ICreateTypeLib)
-        GetInterfaceFromGlobal(cookie)
-        self.assertEqual((tlib.AddRef(), tlib.Release()), (3, 2))
-        RevokeInterfaceFromGlobal(cookie)
+        with register_in_global(tlib) as cookie:
+            # When an object is registered to GIT, `AddRef` is called,
+            # incrementing its reference count. This ensures the object
+            # remains valid as long as it's globally registered in the GIT.
+            self.assertEqual((tlib.AddRef(), tlib.Release()), (3, 2))
+            GetInterfaceFromGlobal(cookie, interface=ICreateTypeLib)
+            GetInterfaceFromGlobal(cookie, interface=ICreateTypeLib)
+            GetInterfaceFromGlobal(cookie)
+            self.assertEqual((tlib.AddRef(), tlib.Release()), (3, 2))
         # When an object is revoked from the GIT, `Release` is called,
         # decrementing its reference count. This allows the object to be
         # garbage collected if no other references exist, ensuring proper
